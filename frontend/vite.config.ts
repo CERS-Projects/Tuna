5e77c07ceb0fcb8a68598cf9794b25bf812da8bf
hmuto111 <h.muto111@gmail.com>
2025-10-16 15:39:57 +0900

    chore: vscode設定の追加、tsconfigpathsの追加 #3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: "0.0.0.0",
     port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 });
