b6f4ef95bd01e4030f8ee957107c9e1d8e22caf1
hmuto111 <h.muto111@gmail.com>
2025-11-27 11:57:11 +0900

    feat: modalコンポーネントを実装 #58

---
+++ frontend/src/components/ui/modal/modal.tsx
@@ -0,0 +1,55 @@
+import React, { useImperativeHandle, useRef, useCallback } from "react";
+import styles from "./modal.module.css";
+
+export type ModalHandle = {
+  show: () => void;
+  close: () => void;
+};
+
+type ModalProps = React.DialogHTMLAttributes<HTMLDialogElement> & {
+  ref?: React.Ref<ModalHandle>;
+  width?: string | number;
+  height?: string | number;
+  children: React.ReactNode;
+};
+
+export const Modal = ({
+  ref,
+  width,
+  height,
+  children,
+  ...props
+}: ModalProps) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const showModal = useCallback(() => {
+    if (modalRef.current) {
+      modalRef.current.showModal();
+    }
+  }, []);
+
+  const closeModal = useCallback(() => {
+    if (modalRef.current) {
+      modalRef.current.close();
+    }
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    show: () => showModal(),
+    close: () => closeModal(),
+  }));
+
+  return (
+    <dialog
+      className={styles.modal}
+      style={{ width, height }}
+      ref={modalRef}
+      onClick={closeModal}
+      {...props}
+    >
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </dialog>
+  );
+};
