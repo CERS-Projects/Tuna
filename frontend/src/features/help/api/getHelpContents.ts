b4dad0d85531ea97e963344a5f1b1d403889c9db
hmuto111 <h.muto111@gmail.com>
2025-12-08 13:38:55 +0900

    feat: help取得処理に返却値の型を定義 #85

--- frontend/src/features/help/api/getHelpContents.ts
+++ frontend/src/features/help/api/getHelpContents.ts
@@ -1,6 +1,8 @@
 import { type HelpContentsType } from "../types/helpContents";
 
-export const getHelpContents = async (categoryId: number) => {
+export const getHelpContents = async (
+  categoryId: number
+): Promise<HelpContentsType[]> => {
   const response = await fetch(
     `${import.meta.env.VITE_API_BASE_URL}/support/help/category/${categoryId}`,
     { method: "GET" }
