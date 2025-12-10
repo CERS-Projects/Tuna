0f778d9197dfa0f1258c51b07f9fff36367ab1c2
MattunS0 <mqtsumoto.s0u@gmail.com>
2025-12-10 14:15:27 +0900

    refactor: helpクラスで使われているフィールドをわかりやすくしました#77

---
+++ backend/src/main/java/com/example/backend/accounts/helper/AccountsHelper.java
@@ -0,0 +1,32 @@
+package com.example.backend.accounts.helper;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.backend.accounts.model.UserEntity;
+
+import lombok.RequiredArgsConstructor;
+
+/*
+ * DtoとEntityの変換部分の共通部分の処理(Helper method)
+ */
+
+@RequiredArgsConstructor
+public class AccountsHelper {
+    
+    private final PasswordEncoder passwordEncoder;
+
+    /* UserEntityに変換 */
+    public UserEntity toUserEntity(Integer schoolId, String showUserId, String name, String mailAddress, String password){
+        
+        UserEntity newUserAccount = new UserEntity();
+        String digest = passwordEncoder.encode(password);
+
+        newUserAccount.setSchoolId(schoolId);
+        newUserAccount.setShowUserId(showUserId);
+        newUserAccount.setName(name);
+        newUserAccount.setPassword(digest);
+        newUserAccount.setMailAddress(mailAddress);
+
+        return newUserAccount;
+    }
+}
