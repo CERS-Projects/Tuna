f1e45a8f8632c664bbf324dea409cd5eeb30a688
productD5 <daigo.densiaddress@gmail.com>
2025-12-10 14:16:10 +0900

    fix : 問い合わせのAPIエンドポイントの修正、不要なインポートの削除 feature #64

--- backend/src/main/java/com/example/backend/support/controller/InquiryController.java
+++ backend/src/main/java/com/example/backend/support/controller/InquiryController.java
@@ -4,31 +4,30 @@ import com.example.backend.support.service.InquiryService;
 import com.mongodb.lang.NonNull;
 
 import lombok.RequiredArgsConstructor;
-
 import com.example.backend.support.dto.InquiryInsertRequest;
 import com.example.backend.support.dto.InquiryUpdateRequest;
-import com.example.backend.support.dto.InquiryDeleteRequest;
 import com.example.backend.support.model.InquiryEntity;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Date;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/inquiry")
+@RequestMapping("inquiries")
 public class InquiryController {
 
     private final InquiryService inquiryService;
@@ -75,10 +74,10 @@ public class InquiryController {
         }
     }
 
-    @GetMapping("/findById")
-    public ResponseEntity<InquiryEntity> getInquiryById(@RequestParam  @NonNull String _id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<InquiryEntity> getInquiryById(@PathVariable("id") @NonNull String id) {
         try {
-            InquiryEntity inquiry = inquiryService.findByInquiryId(_id);
+            InquiryEntity inquiry = inquiryService.findByInquiryId(id);
             if (inquiry != null) {
                 return ResponseEntity.status(HttpStatus.OK).body(inquiry);
             } else {
@@ -92,8 +91,8 @@ public class InquiryController {
     }
     
 
-    @PutMapping("/updateStatus")
-    public ResponseEntity<Void> postMethodName(@RequestBody InquiryUpdateRequest inquiry) {
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> postMethodName(@PathVariable("id") @NonNull String _id, @RequestBody InquiryUpdateRequest inquiry) {
         try {
             String id = inquiry.get_id();
             Integer status = inquiry.getInquiry_status();
@@ -107,11 +106,11 @@ public class InquiryController {
     }
     
 
-    @PostMapping("/delete")
-    public ResponseEntity<Void> deleteInquiry(@RequestBody  InquiryDeleteRequest _id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteInquiry(@PathVariable("id") @NonNull String id) {
         try {
-            inquiryService.deleteInquiry( _id.get_id() );
-            System.out.println("Deleted inquiry with id: " + _id);
+            inquiryService.deleteInquiry( id );
+            System.out.println("Deleted inquiry with id: " + id);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } catch (Exception e) {
             // エラーハンドリング（例：ログ出力、適切なレスポンスコードの返却など）
