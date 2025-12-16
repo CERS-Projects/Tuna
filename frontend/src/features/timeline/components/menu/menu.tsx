769281c8b9a1693ae5679ace61a03dd59e762311
nukomaru0930 <oboronagi0930@icloud.com>
2025-12-16 10:47:26 +0900

    refactor: ダミーデータの削除、cssの調整、usenavigateの使用 #90

--- frontend/src/features/timeline/components/menu/menu.tsx
+++ frontend/src/features/timeline/components/menu/menu.tsx
@@ -3,29 +3,31 @@ import { IoSchoolOutline } from "react-icons/io5";
 import { LuCirclePlus } from "react-icons/lu";
 import { AiOutlineSolution } from "react-icons/ai";
 import { BsBookmark } from "react-icons/bs";
-import { Navigate } from "react-router";
+import { useNavigate } from "react-router";
 import { PiHouseLight } from "react-icons/pi";
 import styles from "./menu.module.css";
 
 export const Menu = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.menuContainer}>
-      <button onClick={() => Navigate("")}>
+      <button onClick={() => navigate("")}>
         <PiHouseLight />
       </button>
-      <button onClick={() => Navigate("")} className={styles.menuButton}>
+      <button onClick={() => navigate("")} className={styles.menuButton}>
         <HiOutlineMagnifyingGlass />
       </button>
-      <button onClick={() => Navigate("")} className={styles.menuButton}>
+      <button onClick={() => navigate("")} className={styles.menuButton}>
         <IoSchoolOutline />
       </button>
-      <button onClick={() => Navigate("")} className={styles.menuButton}>
+      <button onClick={() => navigate("")} className={styles.menuButton}>
         <LuCirclePlus />
       </button>
-      <button onClick={() => Navigate("")} className={styles.menuButton}>
+      <button onClick={() => navigate("")} className={styles.menuButton}>
         <AiOutlineSolution />
       </button>
-      <button onClick={() => Navigate("")} className={styles.menuButton}>
+      <button onClick={() => navigate("")} className={styles.menuButton}>
         <BsBookmark />
       </button>
     </div>
