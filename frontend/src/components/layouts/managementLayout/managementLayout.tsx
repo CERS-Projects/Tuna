cbd752cc9c3348b993b3f22dc0c0978eba66dfbd
hmuto111 <h.muto111@gmail.com>
2025-12-17 11:36:48 +0900

    feat: 960px以下のレイアウトを実装、クラス名の変更 #100

--- frontend/src/components/layouts/managementLayout/managementLayout.tsx
+++ frontend/src/components/layouts/managementLayout/managementLayout.tsx
@@ -3,7 +3,7 @@ import { Menu } from "@/features/timeline/components/menu/menu";
 import { MenuDrawer } from "@/features/management/components/menuDrawer/menuDrawer";
 import { FaChevronRight } from "react-icons/fa";
 import styles from "./managementLayout.module.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -11,28 +11,68 @@ type Props = {
 
 export const ManagementLayout = ({ children }: Props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 960) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className={styles.layoutContainer}>
       <Header />
 
-      <div className={styles.container}>
-        <aside className={styles.menuContainer}>
+      {!isMobile ? (
+        <div className={styles.container}>
+          <aside className={styles.menuContainer}>
+            <div className={styles.menuWrapper}>
+              <Menu />
+            </div>
+            <button
+              onClick={() => setIsOpen(true)}
+              className={styles.openDrawer}
+              style={isOpen ? { display: "none" } : { display: "flex" }}
+            >
+              <FaChevronRight />
+            </button>
+            <MenuDrawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
+          </aside>
+
+          <main className={styles.content}>{children}</main>
+        </div>
+      ) : (
+        // 960px以下のレイアウト
+        <div className={styles.container}>
+          <div className={styles.contentContainer}>
+            <button
+              onClick={() => setIsOpen(true)}
+              className={styles.openDrawer}
+              style={isOpen ? { display: "none" } : { display: "flex" }}
+            >
+              <FaChevronRight />
+            </button>
+            <MenuDrawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
+            <main className={styles.main}>{children}</main>
+          </div>
+
           <div className={styles.menuWrapper}>
             <Menu />
           </div>
-          <button
-            onClick={() => setIsOpen(true)}
-            className={styles.openDrawer}
-            style={isOpen ? { display: "none" } : { display: "flex" }}
-          >
-            <FaChevronRight />
-          </button>
-          <MenuDrawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
-        </aside>
-
-        <main className={styles.content}>{children}</main>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
