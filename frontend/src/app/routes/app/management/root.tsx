70003a84e00f4e104f0c62b90e60ceebe1c810bd
hmuto111 <h.muto111@gmail.com>
2025-12-17 14:18:44 +0900

    feat: errorboudryの表示されるメッセージを書きました #100

--- frontend/src/app/routes/app/management/root.tsx
+++ frontend/src/app/routes/app/management/root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import { ManagementLayout } from "@/components/layouts/managementLayout/managementLayout";
 
 export const ErrorBoundary = () => {
-  return <div>Something went wrong!</div>;
+  return <div>エラーが発生しました。管理画面の表示に問題が発生しました。</div>;
 };
 
 const ManagementRoot = () => {
