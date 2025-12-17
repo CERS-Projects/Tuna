b235ed64854d4e90b6b2604039c7276da6f4e62f
productD5 <daigo.densiaddress@gmail.com>
2025-12-17 12:09:55 +0900

    feat : CSVのバリデーション項目を追加 feature #98

--- backend/src/main/java/com/example/backend/utils/fileUtil/validation/DocumentFileValidation.java
+++ backend/src/main/java/com/example/backend/utils/fileUtil/validation/DocumentFileValidation.java
@@ -15,7 +15,7 @@ public class DocumentFileValidation {
         }
         try {
             file.getInputStream().read(header);
-            return isPdf(header) || isDocx(header) || isXlsx(header) || isPptx(header);
+            return isPdf(header) || isDocx(header) || isXlsx(header) || isPptx(header) || isCsv(header) ;
         } catch (Exception e) {
             return false;
         }
@@ -35,7 +35,11 @@ public class DocumentFileValidation {
     // PPTX: 50 4B 03 04 14
     private boolean isPptx(byte[] h) {
         return h[0] == 0x50 && h[1] == 0x4B && h[2] == 0x03 && h[3] == 0x04 && h[4] == 0x14;
-    }  
+    }
     
+    //CSV: 23 63 6F 6D 6D
+    private boolean isCsv(byte[] h) {
+        return h[0] == 0x23 && h[1] == 0x63 && h[2] == 0x6F && h[3] == 0x6D && h[4] == 0x6D;
+    }
 
 }
