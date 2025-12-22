3c71bb5c9eb083ad30ef241c87aa90039489b567
nukomaru0930 <oboronagi0930@icloud.com>
2025-12-22 14:28:34 +0900

    feat:タイムライン関係のレイアウトの作成と修正feature/#112

--- frontend/src/app/routes/app/timeline.tsx
+++ frontend/src/app/routes/app/timeline.tsx
@@ -1,7 +1,5 @@
-import { Header } from "@/components/ui/header/header";
 import { InfoBox } from "@/components/ui/infoBox/infoBox";
 import { TimelineFilter } from "@/features/timeline/components/timelineFilter/timelineFilter";
-import { Menu } from "@/features/timeline/components/menu/menu";
 import { PostBox } from "@/components/ui/postBox/postBox";
 import { RiCompass3Line } from "react-icons/ri";
 import styles from "@/features/timeline/styles/timeline.module.css";
@@ -83,12 +81,8 @@ const Timeline = () => {
   };
   return (
     <div className={styles.timelineLayout}>
-      <Header />
       <div className={styles.timelineContainer}>
         <div className={styles.timelineAreaLeft}>
-          <Menu />
-        </div>
-        <div className={styles.timelineAreaCenter}>
           <button onClick={modalButtonClick} className={styles.modalButton}>
             <RiCompass3Line />
           </button>
---
+++ frontend/src/components/layouts/appLayout/appLayout.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@/components/ui/header/header";
+import { Menu } from "@/features/timeline/components/menu/menu";
+import styles from "./appLayout.module.css";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const AppLayout = ({ children }: Props) => {
+  return (
+    <div className={styles.appLayoutContainer}>
+      <Header />
+      <div className={styles.appLayoutArea}>
+        <div className={styles.appLayoutAreaLeft}>
+          <Menu />
+        </div>
+        <div className={styles.appLayoutAreaRight}>{children}</div>
+      </div>
+    </div>
+  );
+};
