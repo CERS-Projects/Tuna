05c0091bdb882a87b24ab40d508689a83329e282
nukomaru0930 <oboronagi0930@icloud.com>
2025-12-24 15:54:00 +0900

    refactor:cssのクラスネーム付け忘れてたので修正しました feature/#112

--- frontend/src/app/routes/app/timeline.tsx
+++ frontend/src/app/routes/app/timeline.tsx
@@ -82,7 +82,7 @@ const Timeline = () => {
   return (
     <div className={styles.timelineLayout}>
       <div className={styles.timelineContainer}>
-        <div className={styles.timelineContainerAreaLeft}>
+        <div className={styles.timelineContainerLeft}>
           <button onClick={modalButtonClick} className={styles.modalButton}>
             <RiCompass3Line />
           </button>
@@ -103,7 +103,7 @@ const Timeline = () => {
             />
           ))}
         </div>
-        <div className={styles.timelineContainerAreaRight}>
+        <div className={styles.timelineContainerRight}>
           <InfoBox>
             <TimelineFilter />
           </InfoBox>
