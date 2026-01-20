ec5bc4b95aae955afec6a8423768d5db80ddde1b
yuki <yuki.kikuchi27@gmail.com>
2026-01-20 11:44:20 +0900

    feat: バックエンドで毎回通過するFilter機能の実装 78

---
+++ backend/src/main/java/com/example/backend/config/AuthenticationFilter.java
@@ -0,0 +1,80 @@
+package com.example.backend.config;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.backend.auth.service.LoginAttemptService;
+import com.example.backend.utils.jwt.JwtUtils;
+import org.springframework.lang.NonNull;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final LoginAttemptService loginAttemptService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        System.out.println("doFilter通過");
+        String sub;
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        // トークンがないとき
+        if (header == null || header.isBlank()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // トークンがあってもベアラートークンではない時
+        if (!header.startsWith("Bearer ")) {
+            request.setAttribute("ERROR_MESSAGE", "トークンが不正です");
+            throw new BadCredentialsException("");
+        }
+        String token = header.substring(7);
+        try {
+            DecodedJWT successToken = jwtUtils.confirmToken(token);
+            sub = successToken.getSubject();
+            String role = successToken.getClaim("role").asString();
+            List<GrantedAuthority> authority = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(sub, null, authority);
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+            System.out.println("ROLE = " + role);
+            System.out.println("AUTHORITIES = " + authority);
+
+        } catch (TokenExpiredException e) {
+            request.setAttribute("ERROR_MESSAGE", "トークンの有効切れです");
+            throw new BadCredentialsException("");
+        } catch (JWTVerificationException e) {
+            request.setAttribute("ERROR_MESSAGE", "トークンが不正です");
+            throw new BadCredentialsException("");
+        }
+        System.out.println("アカウント停止確認直前");
+        // アカウント停止フラグチェック
+        loginAttemptService.isStop(request, sub);
+        System.out.println("アカウント停止確認後");
+        filterChain.doFilter(request, response);
+    }
+}
