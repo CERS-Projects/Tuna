1ab4757de8830fa3d1ea566ba32df99de435340c
productD5 <daigo.densiaddress@gmail.com>
2026-01-20 11:21:27 +0900

    feat : 投稿作成、取得(3つ)、削除の機能実装完了 feature#65

--- backend/src/main/java/com/example/backend/posts/controller/PostController.java
+++ backend/src/main/java/com/example/backend/posts/controller/PostController.java
@@ -1,11 +1,16 @@
 package com.example.backend.posts.controller;
 
-import com.example.backend.posts.dto.PostDetailDto;
-import com.example.backend.posts.dto.GetTimelineRequestDto;
-import com.example.backend.posts.dto.PostInsertRequestDto;
+import com.example.backend.posts.dto.TimelinePostsRequest;
+import com.example.backend.posts.dto.ProfilePostsRequest;
+import com.example.backend.posts.dto.PostInsertRequest;
+import com.example.backend.posts.dto.PostDetailResponse;
+import com.example.backend.posts.dto.PostsReplyRequest;
+import com.example.backend.posts.model.PostEntity;
+import com.example.backend.posts.model.BookmarkEntity;
 
 import com.example.backend.posts.model.PostEntity;
-import com.example.backend.posts.service.impl.PostServiceImpl;
+import com.example.backend.posts.service.PostService;
+import com.example.backend.posts.service.BookmarkService;
 import lombok.extern.log4j.Log4j2;
 
 import lombok.RequiredArgsConstructor;
@@ -33,13 +38,15 @@ import org.slf4j.LoggerFactory;
 @RequiredArgsConstructor
 public class PostController {
     
-    private final PostServiceImpl postService;
+    private final PostService postService;
+    private final BookmarkService bookmarkService;
 
     //投稿を作成
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Void> insertPost(@Valid @ModelAttribute PostInsertRequestDto postRequest) { 
+    public ResponseEntity<Void> insertPost(@Valid @ModelAttribute PostInsertRequest postRequest) { 
         // @ModelAttributeを使うことで、画像とテキストを一つのDTOで受け取れます
         try {
+            log.info("投稿の作成を開始しました。 "+ postRequest);
             postService.insertPost(postRequest);
             return ResponseEntity.ok().build();
         } catch (Exception e) {
@@ -50,8 +57,8 @@ public class PostController {
 
     //タイムライン投稿を取得
     @GetMapping("/timeline")
-    public ResponseEntity<List<PostDetailDto>> getTimelinePosts(@Valid @ModelAttribute GetTimelineRequestDto requestDto){
-        List<PostDetailDto> timelinePosts;
+    public ResponseEntity<List<PostDetailResponse>> getTimelinePosts(@Valid @RequestBody TimelinePostsRequest requestDto){
+        List<PostDetailResponse> timelinePosts;
         try{
             timelinePosts = postService.getTimelinePosts(requestDto);
         }catch(Exception e){
@@ -60,6 +67,70 @@ public class PostController {
         }
         
         return ResponseEntity.ok(timelinePosts);
+    }
+
+    //ユーザー投稿を取得
+    @GetMapping("/profile")
+    public ResponseEntity<List<PostDetailResponse>> getUserPosts(@Valid @RequestBody ProfilePostsRequest requestDto){
+        List<PostDetailResponse> userPosts;
+        try{
+            userPosts = postService.getUserPosts(requestDto);
+        }catch(Exception e){
+            log.error("ユーザー投稿の取得に失敗しました。", e);
+            return ResponseEntity.status(500).build();
+        }
+
+        return ResponseEntity.ok(userPosts);
+    }
+    
+    //返信投稿を取得
+    @GetMapping("/responses")
+    public ResponseEntity<List<PostDetailResponse>> getReplyPosts(@Valid @RequestBody PostsReplyRequest requestDto){
+        List<PostDetailResponse> responsePosts;
+        try{
+            responsePosts = postService.getReplyPosts(requestDto);
+        }catch(Exception e){
+            log.error("返信投稿の取得に失敗しました。", e);
+            return ResponseEntity.status(500).build();
+        }
+        return ResponseEntity.ok(responsePosts);
+    }
+    
+    //ブックマーク追加
+    @PostMapping("/bookmarks/add")
+    public ResponseEntity<Void> addBookmark(@Valid @RequestBody BookmarkEntity bookmark) {
+        try {
+            bookmarkService.addBookmark(bookmark);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.error("ブックマークの追加に失敗しました。", e);
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    //ブックマーク削除
+    @PostMapping("/bookmarks/remove")
+    public ResponseEntity<Void> removeBookmark(@Valid @RequestBody BookmarkEntity bookmark) {
+        try {
+            bookmarkService.removeBookmark(bookmark);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.error("ブックマークの削除に失敗しました。", e);
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    //ブックマーク取得
+    @GetMapping("/bookmarks/{userId}")
+    public ResponseEntity<List<PostDetailResponse>> getBookmarkedPosts(@PathVariable Integer userId){
+        List<PostDetailResponse> bookmarkedPosts;
+        try{
+            bookmarkedPosts = bookmarkService.getBookmarkedPosts(userId);
+        }catch(Exception e){
+            log.error("ブックマーク投稿の取得に失敗しました。", e);
+            return ResponseEntity.status(500).build();
+        }
+        return ResponseEntity.ok(bookmarkedPosts);
     }
     
     //投稿を削除
