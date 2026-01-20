cc5d59534dafa253cc6bc0e0df2917fec672be72
productD5 <daigo.densiaddress@gmail.com>
2026-01-20 11:33:34 +0900

    feat : 投稿のサービスを実装、作成、取得、削除を作成 feature #65

--- backend/src/main/java/com/example/backend/posts/service/impl/PostServiceImpl.java
+++ backend/src/main/java/com/example/backend/posts/service/impl/PostServiceImpl.java
@@ -1,14 +1,19 @@
 package com.example.backend.posts.service.impl;
 
-import com.example.backend.posts.dto.GetTimelineRequestDto;
-import com.example.backend.posts.dto.GetProfilePostsRequestDto;
-import com.example.backend.posts.dto.PostInsertRequestDto;
-import com.example.backend.posts.dto.PostDetailDto;
+import com.example.backend.posts.dto.TimelinePostsRequest;
+import com.example.backend.posts.dto.ProfilePostsRequest;
+import com.example.backend.posts.dto.PostInsertRequest;
+import com.example.backend.posts.dto.PostDetailResponse;
+import com.example.backend.posts.dto.PostsReplyRequest;
 import com.example.backend.posts.model.PostEntity;
+import org.bson.types.ObjectId;
+
+
 
 import com.example.backend.posts.repository.PostRepository;
 import com.example.backend.posts.repository.LikeRepository;
 import com.example.backend.posts.repository.BookmarkRepository;
+import com.example.backend.posts.model.PostEntity;
 
 import com.example.backend.posts.service.PostService;
 import com.example.backend.utils.fileUtil.helper.FileControlHelper;
@@ -24,6 +29,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.multipart.MultipartFile;
 
 @Log4j2
 @Service
@@ -34,73 +40,119 @@ public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
 
     private final FileControlHelper fileControlHelper;
-
-    private final LikeRepository likeRepository;
-
-    private final BookmarkRepository bookmarkRepository;
     
     // 投稿作成
-    public void insertPost(PostInsertRequestDto post) {
+    public void insertPost(PostInsertRequest post) {
         PostEntity postEntity = new PostEntity();
-        List<String> fileObjectKey = null;
+        List<String> fileObjectKeys = null;
+        List<MultipartFile> imageFiles = post.getImageFile();
+        ObjectId replyPost = null;
 
-        if(post.getImageFile() != null){
-            fileObjectKey = fileControlHelper.uploadFile("images",post.getImageFile());
-            log.info("ファイルアップロード成功: " + String.join(", ", fileObjectKey));
+        if(imageFiles != null){
+            fileObjectKeys = fileControlHelper.uploadFile("images", imageFiles.toArray(new MultipartFile[0]));
+            log.info("ファイルアップロード成功: {}", String.join(", ", fileObjectKeys));
+            
+        }
+        //返信投稿設定
+        if (post.getResponseTo() != null && !post.getResponseTo().isEmpty()) {
+            replyPost = new ObjectId(post.getResponseTo()); 
+            log.info("投稿が{}", postRepository.existsById(replyPost.toHexString()));
         }
 
         OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
         postEntity.setUserId(post.getUser_id());
         postEntity.setSentence(post.getSentence());
         postEntity.setPostDate(Date.from(now.toInstant()));
-        postEntity.setImageObjectKey(fileObjectKey);
+        postEntity.setImageObjectKey(fileObjectKeys);
         postEntity.setShareRange(post.getShareRange());
-        //初期値はfalse
-        postEntity.setPostFlag(false);
+        //初期値はtrue
+        postEntity.setPostFlag(true);
         postEntity.setLikeCount(0);
-        postEntity.setResponseTo(null);
-
+        postEntity.setResponseTo(replyPost);
+        postEntity.setResponseCount(0);
         try {
         postRepository.save(postEntity);
+        if (postEntity.getResponseTo() != null) {
+                log.info("返信元投稿のresponse_Countをインクリメントします。 投稿ID: {}", replyPost.toHexString());
+                long result = postRepository.incrementResponseCount(replyPost);
+                if (result == 0) {
+                    log.warn("返信元投稿のresponse_Countのインクリメントに失敗しました。該当する投稿が見つかりません。 投稿ID:{} " , replyPost.toHexString());
+                    throw new RuntimeException("返信元投稿のresponse_Countのインクリメントに失敗しました。該当する投稿が見つかりません。");
+                } else {
+                    log.info("返信元投稿のresponse_Countを正常にインクリメントしました。 投稿ID: {}", replyPost.toHexString());
+                }
+            }
         } catch (Exception e) {
             // ファイル削除
-            if(fileObjectKey != null){
-                fileControlHelper.deleteFile(fileObjectKey.toArray(new String[0]));
+            log.info("投稿の保存に失敗したため、アップロードしたファイルを削除します。");
+            if(fileObjectKeys != null){
+                fileControlHelper.deleteFile(fileObjectKeys.toArray(new String[0]));
             }
             log.error("投稿の保存に失敗しました。", e);
             throw new RuntimeException("投稿の保存に失敗しました。", e);
         }
     }
 
-    //タイムライン投稿取得
-    public List<PostDetailDto> getTimelinePosts(GetTimelineRequestDto requestDto) {
-        List<PostDetailDto> postDetails;
-        try{
-            postDetails = postRepository.findPostsWithDetails(requestDto.getUserId(), requestDto.getShareRange(), requestDto.getMuteWords());
-            log.info("画像" + postDetails.get(0).getImageUrl());
-        }catch(Exception e){
+    //タイムライン取得
+    public List<PostDetailResponse> getTimelinePosts(TimelinePostsRequest requestDto) {
+        try {
+            List<PostDetailResponse> postDetails =
+                postRepository.findPostsWithDetails(
+                    requestDto.getUserId(),
+                    requestDto.getShareRange(),
+                    requestDto.getMuteWords()
+                );
+
+            log.info("タイムライン投稿 件数={}", postDetails.size());
+            if (!postDetails.isEmpty()) {
+                log.info("画像(先頭)={}", postDetails.get(0).getImageUrl());
+            }
+
+            for (PostDetailResponse postDetail : postDetails) {
+                postDetail.setImageUrl(fileControlHelper.getMultiFileUrl(postDetail.getImageUrl()));
+                postDetail.setIcon(fileControlHelper.getFileUrl(postDetail.getIcon()));
+            }
+
+            return postDetails;
+
+        } catch (Exception e) {
             log.error("タイムライン投稿の取得に失敗しました。", e);
             throw new RuntimeException("タイムライン投稿の取得に失敗しました。", e);
         }
-        for(PostDetailDto postDetail : postDetails){
+    }
+
+    //ユーザー投稿取得
+    public List<PostDetailResponse> getUserPosts(ProfilePostsRequest requestDto) {
+        List<PostDetailResponse> postDetails;
+        try{
+            log.info("取得を開始しました 相手targetUserId: " + requestDto.getTargetUserId() + " 取得 currentUserId: " + requestDto.getCurrentUserId() );
+            postDetails = postRepository.findUserPostsWithDetails(requestDto.getCurrentUserId(), requestDto.getTargetUserId(), requestDto.getGroupIds());
+            log.info("取得完了しました。 投稿数: " + postDetails);
+        }catch(Exception e){
+
+            log.error("ユーザー投稿の取得に失敗しました。", e.getMessage(), e);
+            e.printStackTrace();
+            throw new RuntimeException("ユーザー投稿の取得に失敗しました。", e);
+        }
+        for(PostDetailResponse postDetail : postDetails){
             postDetail.setImageUrl(fileControlHelper.getMultiFileUrl(postDetail.getImageUrl()));
             postDetail.setIcon(fileControlHelper.getFileUrl(postDetail.getIcon()));
         }
 
         return postDetails;
-        }
-
-    //ユーザー投稿取得
-    public List<PostDetailDto> getUserPosts(GetProfilePostsRequestDto requestDto) {
-        List<PostDetailDto> postDetails;
+    }
+    //返信取得
+    public List<PostDetailResponse> getReplyPosts(PostsReplyRequest requestDto) {
+        List<PostDetailResponse> postDetails;
         try{
-            log.info("取得を開始しました 相手targetUserId: " + requestDto.getTargetUserId() + " 取得 currentUserId: " + requestDto.getCurrentUserId() );
-            postDetails = postRepository.findUserPostsWithDetails(requestDto.getCurrentUserId(), requestDto.getTargetUserId(), requestDto.getGroupIds());
+            log.info("返信取得を開始しました 投稿ID: " + requestDto.getReplyPostId() + " 取得 currentUserId: " + requestDto.getCurrentUserId() );
+            postDetails = postRepository.findPostsResponseWithDetails(requestDto.getCurrentUserId(), requestDto.getReplyPostId(), requestDto.getMuteWords());
         }catch(Exception e){
-            log.error("ユーザー投稿の取得に失敗しました。", e);
-            throw new RuntimeException("ユーザー投稿の取得に失敗しました。", e);
+            log.error("返信投稿の取得に失敗しました。", e.getMessage(), e);
+            e.printStackTrace();
+            throw new RuntimeException("返信投稿の取得に失敗しました。", e);
         }
-        for(PostDetailDto postDetail : postDetails){
+        for(PostDetailResponse postDetail : postDetails){
             postDetail.setImageUrl(fileControlHelper.getMultiFileUrl(postDetail.getImageUrl()));
             postDetail.setIcon(fileControlHelper.getFileUrl(postDetail.getIcon()));
         }
