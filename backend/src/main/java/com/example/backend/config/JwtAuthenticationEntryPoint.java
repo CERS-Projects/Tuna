7421b6aab545899e81b86a6cc0eeb11ebc3818e1
yuki <yuki.kikuchi27@gmail.com>
2026-01-20 11:47:54 +0900

    feat: 認証で発生するエラー処理実装 #78

---
+++ backend/src/main/java/com/example/backend/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,42 @@
+package com.example.backend.config;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.example.backend.exception.Model.ErrorResponseEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException {
+        String authErrorMessage;
+        if (request.getAttribute("ERROR_MESSAGE") == null) {
+            authErrorMessage = "ログインしなおしてください";
+        } else {
+            authErrorMessage = request.getAttribute("ERROR_MESSAGE").toString();
+        }
+
+        ErrorResponseEntity errorResponseEntity = new ErrorResponseEntity(HttpStatus.UNAUTHORIZED.value(),
+                authErrorMessage);
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponseEntity));
+
+    }
+
+}
