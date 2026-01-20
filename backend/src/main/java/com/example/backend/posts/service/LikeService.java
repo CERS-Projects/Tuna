4a1eae75d6f5454682dd25d9919ec11dacea68c5
productD5 <daigo.densiaddress@gmail.com>
2026-01-20 11:31:00 +0900

    feat : いいねサービス用のインターフェースを作成　feature #65

--- backend/src/main/java/com/example/backend/posts/service/LikeService.java
+++ backend/src/main/java/com/example/backend/posts/service/LikeService.java
@@ -1,6 +1,6 @@
 package com.example.backend.posts.service;
 
-import com.example.backend.posts.dto.PostDetailDto;
+import com.example.backend.posts.dto.PostDetailResponse;
 import java.util.List;
 
 public interface LikeService {
