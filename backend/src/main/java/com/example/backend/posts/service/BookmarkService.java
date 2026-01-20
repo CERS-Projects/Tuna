e5b70b2828aa57ab31a3e680c2e3b81f0f028248
productD5 <daigo.densiaddress@gmail.com>
2026-01-20 11:30:40 +0900

    feat : postService用のインターフェースを作成　feature #65

--- backend/src/main/java/com/example/backend/posts/service/BookmarkService.java
+++ backend/src/main/java/com/example/backend/posts/service/BookmarkService.java
@@ -1,8 +1,13 @@
 package com.example.backend.posts.service;
 
-import com.example.backend.posts.dto.PostDetailDto;
+import com.example.backend.posts.dto.PostDetailResponse;
 import java.util.List;
 
 public interface BookmarkService {
 
+    void addBookmark(com.example.backend.posts.model.BookmarkEntity bookmark);
+
+    void removeBookmark(com.example.backend.posts.model.BookmarkEntity bookmark);
+
+    List<PostDetailResponse> getBookmarkedPosts(Integer userId);
 }
