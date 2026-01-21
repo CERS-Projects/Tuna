a3bbd990f7f2a579950e6c42239692a7571acb73
productD5 <daigo.densiaddress@gmail.com>
2026-01-21 11:31:47 +0900

    feat : 実装する関数を正式に追加 feature #65

--- backend/src/main/java/com/example/backend/posts/service/LikeService.java
+++ backend/src/main/java/com/example/backend/posts/service/LikeService.java
@@ -1,9 +1,17 @@
 package com.example.backend.posts.service;
 
 import com.example.backend.posts.dto.PostDetailResponse;
+import com.example.backend.posts.model.LikeEntity;
+import com.example.backend.posts.dto.IsLikeRequest;
 import java.util.List;
 
 public interface LikeService {
+
+    void addLikes(IsLikeRequest requestDto);
+
+    void removeLikes(IsLikeRequest requestDto);
+
+    List<PostDetailResponse> getLikedPosts(Integer userId);
 
 
 }
