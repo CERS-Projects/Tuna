c1667a4c151960cb72589ec7342b3165ae05fdee
MattunS0 <mqtsumoto.s0u@gmail.com>
2026-01-21 11:01:48 +0900

    chore:StudentServiceのコンフリクトの修正 #137

--- backend/src/main/java/com/example/backend/accounts/service/StudentService.java
+++ backend/src/main/java/com/example/backend/accounts/service/StudentService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.backend.accounts.dto.StudentCreateRequest;
+import com.example.backend.accounts.dto.StudentInformationResponses;
 import com.example.backend.accounts.model.UserEntity;
 import com.example.backend.group.dto.GetUserBySchoolId;
 import com.example.backend.group.dto.GetUserResponse;
@@ -14,5 +15,6 @@ public interface StudentService {
     UserEntity createStudent(StudentCreateRequest dto);
     void setStudentEnrollmentInformation(StudentCreateRequest dto, UserEntity savedStudentAccount);
     void createStudentByFile(MultipartFile csvFile, final Integer schoolId) throws IOException;
-    List<GetUserResponse> findAllGroups(GetUserBySchoolId dto);
+    List<GetUserResponse> findAllGroup(GetUserBySchoolId dto);
+    List<StudentInformationResponses> findStudentInformationResponses();
 }
