b1a00f7e3f245489a10acbfb2e43344a0c3cc361
productD5 <daigo.densiaddress@gmail.com>
2026-01-21 11:31:14 +0900

    fix : サービスのインターフェースの引数を正しいものに修正 feature #65

--- backend/src/main/java/com/example/backend/posts/service/BookmarkService.java
+++ backend/src/main/java/com/example/backend/posts/service/BookmarkService.java
@@ -1,13 +1,15 @@
 package com.example.backend.posts.service;
 
+import com.example.backend.posts.model.BookmarkEntity;
+import com.example.backend.posts.dto.IsBookmarkRequest;
 import com.example.backend.posts.dto.PostDetailResponse;
 import java.util.List;
 
 public interface BookmarkService {
 
-    void addBookmark(com.example.backend.posts.model.BookmarkEntity bookmark);
+    void addBookmark(IsBookmarkRequest requestDto);
 
-    void removeBookmark(com.example.backend.posts.model.BookmarkEntity bookmark);
+    void removeBookmark(IsBookmarkRequest requestDto);
 
     List<PostDetailResponse> getBookmarkedPosts(Integer userId);
 }
