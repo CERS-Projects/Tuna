999de042de63e60806300d2693c8fd7851e1ec72
productD5 <daigo.densiaddress@gmail.com>
2026-01-23 15:42:17 +0900

    feat : 検索履歴用のserviceインターフェースの作成 feature #65

--- backend/src/main/java/com/example/backend/posts/service/SearchHistoryService.java
+++ backend/src/main/java/com/example/backend/posts/service/SearchHistoryService.java
@@ -1,8 +1,16 @@
 package com.example.backend.posts.service;
 
 
+import com.example.backend.posts.model.SearchHistoryEntity;
+import com.example.backend.posts.model.SearchHistoryItem;
+
 import java.util.List;
 
 
 public interface SearchHistoryService {
+    void addSearchHistory(Integer userId, String query);
+
+    List<SearchHistoryItem> getSearchHistory(Integer userId);
+
+    void removeSearchHistory(Integer userId, String query);
 }
