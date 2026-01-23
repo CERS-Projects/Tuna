17a23cf365b52d709450c42c2b31b3346db696e0
productD5 <daigo.densiaddress@gmail.com>
2026-01-23 15:41:48 +0900

    feat : serviceのインターフェースに投稿検索用の関数の追加 feature #65

--- backend/src/main/java/com/example/backend/posts/service/PostService.java
+++ backend/src/main/java/com/example/backend/posts/service/PostService.java
@@ -6,6 +6,7 @@ import com.example.backend.posts.dto.PostDetailResponse;
 import com.example.backend.posts.dto.PostsReplyRequest;
 import com.example.backend.posts.dto.ProfilePostsRequest;
 import com.example.backend.posts.dto.TimelinePostsRequest;
+import com.example.backend.posts.dto.SearchPostsRequest;
 import com.example.backend.posts.dto.PostInsertRequest;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,8 @@ public interface PostService {
     List<PostDetailResponse> getUserPosts(ProfilePostsRequest requestDto);
 
     List<PostDetailResponse> getReplyPosts(PostsReplyRequest requestDto);
+
+    List<PostDetailResponse> getPostsByKeyword(SearchPostsRequest requestDto);
 
     void deletePost(String postId, Integer userId); 
 
