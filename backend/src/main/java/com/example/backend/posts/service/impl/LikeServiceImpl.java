ebd061c306c58a819267c316f609cc02298b8a19
productD5 <daigo.densiaddress@gmail.com>
2026-01-23 15:43:29 +0900

    faet : いいねカウント用の処理を追加

--- backend/src/main/java/com/example/backend/posts/service/impl/LikeServiceImpl.java
+++ backend/src/main/java/com/example/backend/posts/service/impl/LikeServiceImpl.java
@@ -5,13 +5,14 @@ import lombok.RequiredArgsConstructor;
 import com.example.backend.posts.repository.LikeRepository;
 import com.example.backend.posts.dto.IsLikeRequest;
 import com.example.backend.utils.fileUtil.helper.FileControlHelper;
+import com.example.backend.posts.repository.PostCounterRepository;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.List;
 import com.example.backend.posts.dto.PostDetailResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
 import com.example.backend.posts.model.LikeEntity;
 import com.example.backend.posts.service.LikeService;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +22,15 @@ import org.bson.types.ObjectId;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class LikeServiceImpl implements LikeService {
 
     
     private final LikeRepository likeRepository;
 
     private final FileControlHelper fileControlHelper;
+
+    private final PostCounterRepository postCounterRepository;
 
     private boolean isLiked(ObjectId postId, Integer userId) {
         return likeRepository.existsByPostIdAndUserId(postId, userId);
@@ -46,6 +50,7 @@ public class LikeServiceImpl implements LikeService {
         }
         try{
             likeRepository.save(like);
+            postCounterRepository.incrementLikeCount(like.getPostId());
             log.info("いいねが正常に追加されました userId: {} and postId: {}", like.getUserId(), like.getPostId());
 
         } catch(Exception e){
@@ -62,6 +67,7 @@ public class LikeServiceImpl implements LikeService {
         }
         try{
             likeRepository.deleteByPostIdAndUserId(requestDto.getPostId(), requestDto.getUserId());
+            postCounterRepository.decrementLikeCount(requestDto.getPostId());
             log.info("いいねが正常に削除されました userId: {} and postId: {}", requestDto.getUserId(), requestDto.getPostId());
         } catch(Exception e){
             log.error("いいねの削除に失敗しました userId: {} and postId: {} エラー: {}" , requestDto.getUserId(), requestDto.getPostId(), e);
