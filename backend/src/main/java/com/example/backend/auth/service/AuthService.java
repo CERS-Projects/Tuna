0d5a1e64d46f630d99903069cb5a5c566de7e580
yuki <yuki.kikuchi27@gmail.com>
2026-01-26 14:39:02 +0900

    feat: "権限を付与するメソッドを追加" #78

--- backend/src/main/java/com/example/backend/auth/service/AuthService.java
+++ backend/src/main/java/com/example/backend/auth/service/AuthService.java
@@ -1,5 +1,10 @@
 package com.example.backend.auth.service;
 
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import com.example.backend.accounts.model.UserEntity;
 import com.example.backend.auth.dto.LoginSelectRequest;
 import com.example.backend.auth.dto.LoginTokenResponse;
 
@@ -9,5 +14,7 @@ public interface AuthService {
     void logout(Integer userId);
 
     LoginTokenResponse refreshTokenCheck(String refreshToken);
+
+    List<GrantedAuthority> giveAuthority(UserEntity userEntity);
 
 }
