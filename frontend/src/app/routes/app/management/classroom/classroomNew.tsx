15304987f78559a9c2978237687e5d42dfbfc1b6
hmuto111 <h.muto111@gmail.com>
2026-01-26 15:18:27 +0900

    feat: 授業ルーム一覧画面の実装 #147

---
+++ frontend/src/app/routes/app/management/classroom/classroomNew.tsx
@@ -0,0 +1,11 @@
+import styles from "@/features/management/style/classroom.module.css";
+
+const ClassroomNew = () => {
+  return (
+    <div className={styles.contentsContainer}>
+      <h2 className={styles.sectionName}>授業ルーム作成</h2>
+    </div>
+  );
+};
+
+export default ClassroomNew;
