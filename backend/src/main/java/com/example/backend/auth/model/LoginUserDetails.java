40f22f805de43ba7b3f4a817440474ecf4893134
yuki <yuki.kikuchi27@gmail.com>
2026-01-26 13:52:16 +0900

    refactor: "LoginUserDetails.javaリファクタリング" #78

--- backend/src/main/java/com/example/backend/auth/model/LoginUserDetails.java
+++ backend/src/main/java/com/example/backend/auth/model/LoginUserDetails.java
@@ -14,30 +14,37 @@ public class LoginUserDetails implements UserDetails {
     private final UserEntity user;
     private final Collection<? extends GrantedAuthority> authorities;
 
+    @Override
     public String getUsername() {
         return user.getShowUserId();
     }
 
+    @Override
     public String getPassword() {
         return user.getPassword();
     }
 
+    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.authorities;
     }
 
+    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    @Override
     public boolean isEnabled() {
         return true;
     }
