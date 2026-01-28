76871de68dc1be7067aee83e5a2d190bf804206e
productD5 <daigo.densiaddress@gmail.com>
2026-01-28 11:31:12 +0900

    fix :shareRangeの型をlist<integer>からintegerに変更 feature #65

--- backend/src/main/java/com/example/backend/posts/dto/TimelinePostsRequest.java
+++ backend/src/main/java/com/example/backend/posts/dto/TimelinePostsRequest.java
@@ -12,16 +12,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @Setter
 public class TimelinePostsRequest {
 
-    @NotNull
-    @Min(1)
-    private int userId;
-
     @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
     @Size(max = 20, message = "muteWords は最大20個までです")
     private List<@Size(max = 20, message = "muteWords の各要素は20文字以内です") String> muteWords;
 
     @NotNull
     @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
-    private List<Integer> shareRange;
+    private Integer shareRange;
+
+    //jwtから取得するためダミー項目
+    private int userId;
+
+
 
 }
