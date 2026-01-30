fc21866dc6248fc7d82831ea4c016fc0e94de327
productD5 <daigo.densiaddress@gmail.com>
2026-01-30 10:38:11 +0900

    fix : 余計なimportがあったため削除、またエラーハンドリングを使う方式にし、try-catchを削除 feature #65

--- backend/src/main/java/com/example/backend/posts/controller/PostController.java
+++ backend/src/main/java/com/example/backend/posts/controller/PostController.java
@@ -14,7 +14,6 @@ import com.example.backend.posts.model.BookmarkEntity;
 import com.example.backend.posts.dto.IsLikeRequest;
 import com.example.backend.posts.dto.SearchHistoryRemoveRequest;
 
-import com.example.backend.posts.model.PostEntity;
 import com.example.backend.posts.service.PostService;
 import com.example.backend.posts.service.BookmarkService;
 import com.example.backend.posts.service.LikeService;
@@ -35,10 +34,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
-import lombok.RequiredArgsConstructor;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Log4j2
 @RestController
@@ -53,55 +49,42 @@ public class PostController {
     //投稿を作成
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> insertPost(@Valid @ModelAttribute PostInsertRequest postRequest) { 
-        // @ModelAttributeを使うことで、画像とテキストを一つのDTOで受け取れます
-        try {
-            log.info("投稿の作成を開始しました。 "+ postRequest);
-            postService.insertPost(postRequest);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error("投稿の作成に失敗しました。", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        log.info("投稿の作成を開始しました。 "+ "userId: {}, content: {}, imageCount: {}",
+            postRequest.getUserId(),
+            postRequest.getSentence(),
+            postRequest.getImageFile() != null ? postRequest.getImageFile().size() : 0);
+
+        postService.insertPost(postRequest);
+        return ResponseEntity.ok().build();
     }
+
 
     //タイムライン投稿を取得
     @GetMapping("/timeline")
     public ResponseEntity<List<PostDetailResponse>> getTimelinePosts(@Valid @RequestBody TimelinePostsRequest requestDto){
         List<PostDetailResponse> timelinePosts;
-        try{
-            timelinePosts = postService.getTimelinePosts(requestDto);
-        }catch(Exception e){
-            log.error("タイムライン投稿の取得に失敗しました。", e);
-            return ResponseEntity.status(500).build();
-        }
         
+        timelinePosts = postService.getTimelinePosts(requestDto);       
         return ResponseEntity.ok(timelinePosts);
     }
+
 
     //ユーザー投稿を取得
     @GetMapping("/profile")
     public ResponseEntity<List<PostDetailResponse>> getUserPosts(@Valid @RequestBody ProfilePostsRequest requestDto){
         List<PostDetailResponse> userPosts;
-        try{
-            userPosts = postService.getUserPosts(requestDto);
-        }catch(Exception e){
-            log.error("ユーザー投稿の取得に失敗しました。", e);
-            return ResponseEntity.status(500).build();
-        }
 
-        return ResponseEntity.ok(userPosts);
+        userPosts = postService.getUserPosts(requestDto);
+        return ResponseEntity.ok(userPosts);    
     }
     
+
     //返信投稿を取得
     @GetMapping("/responses")
     public ResponseEntity<List<PostDetailResponse>> getReplyPosts(@Valid @RequestBody PostsReplyRequest requestDto){
         List<PostDetailResponse> responsePosts;
-        try{
-            responsePosts = postService.getReplyPosts(requestDto);
-        }catch(Exception e){
-            log.error("返信投稿の取得に失敗しました。", e);
-            return ResponseEntity.status(500).build();
-        }
+        
+        responsePosts = postService.getReplyPosts(requestDto);
         return ResponseEntity.ok(responsePosts);
     }
 
@@ -109,90 +92,59 @@ public class PostController {
     @GetMapping("/search")
     public ResponseEntity<List<PostDetailResponse>> getPostsByKeyword(@Valid @RequestBody SearchPostsRequest requestDto){
         List<PostDetailResponse> searchedPosts;
-        try{
-            //検索履歴の追加
-            searchHistoryService.addSearchHistory(requestDto.getCurrentUserId(), requestDto.getKeyword());
-            searchedPosts = postService.getPostsByKeyword(requestDto);
-        }catch(Exception e){
-            log.error("キーワード検索投稿の取得に失敗しました。", e);
-            return ResponseEntity.status(500).build();
-        }
+        
+        //検索履歴の追加
+        searchHistoryService.addSearchHistory(requestDto.getCurrentUserId(), requestDto.getKeyword());
+        searchedPosts = postService.getPostsByKeyword(requestDto);
         return ResponseEntity.ok(searchedPosts);
     }
     
     //ブックマーク追加
     @PostMapping("/addbookmarks")
     public ResponseEntity<Void> addBookmark(@Valid @RequestBody IsBookmarkRequest requestDto) {
-        try {
             bookmarkService.addBookmark(requestDto);
             return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error("ブックマークの追加に失敗しました。", e);
-            return ResponseEntity.status(500).build();
-        }
+        
     }
 
     //ブックマーク削除
     @DeleteMapping("/removebookmarks")
     public ResponseEntity<Void> removeBookmark(@Valid @RequestBody IsBookmarkRequest requestDto) {
-        try {
-            bookmarkService.removeBookmark(requestDto);
-            log.info("ブックマークが正常に削除されました userId: {} and postId: {}", requestDto.getUserId(), requestDto.getPostId());
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error("ブックマークの削除に失敗しました。 userId: {} and postId: {} エラー: {}", requestDto.getUserId(), requestDto.getPostId(), e);
-            return ResponseEntity.status(500).build();
-        }
+    
+        bookmarkService.removeBookmark(requestDto);
+        log.info("ブックマークが正常に削除されました userId: {} and postId: {}", requestDto.getUserId(), requestDto.getPostId());
+        return ResponseEntity.ok().build();
     }
 
     //ブックマーク取得
     @GetMapping("/bookmarks/{userId}")
     public ResponseEntity<List<PostDetailResponse>> getBookmarkedPosts(@PathVariable Integer userId){
         List<PostDetailResponse> bookmarkedPosts;
-        try{
-            bookmarkedPosts = bookmarkService.getBookmarkedPosts(userId);
-        }catch(Exception e){
-            log.error("ブックマーク投稿の取得に失敗しました。 userId: {} エラー: {}", userId, e);
-            return ResponseEntity.status(500).build();
-        }
+        
+        bookmarkedPosts = bookmarkService.getBookmarkedPosts(userId);
         return ResponseEntity.ok(bookmarkedPosts);
     }
 
     //いいね追加
     @PostMapping("/addlikes")
     public ResponseEntity<Void> addLikes(@Valid @RequestBody IsLikeRequest requestDto) {
-        try {
-            likeService.addLikes(requestDto);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error("いいねの追加に失敗しました。 userId: {} and postId: {} エラー: {}", requestDto.getUserId(), requestDto.getPostId(), e);
-            return ResponseEntity.status(500).build();
-        }
+        likeService.addLikes(requestDto);
+        return ResponseEntity.ok().build();
     }
     
     //いいね削除
     @DeleteMapping("/removelikes")
-    public ResponseEntity<Void> removeLikes(@Valid @RequestBody IsLikeRequest requestDto)
-    {
-        try {
-            likeService.removeLikes(requestDto);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error("いいねの削除に失敗しました。 userId: {} and postId: {} エラー: {}", requestDto.getUserId(), requestDto.getPostId(), e);
-            return ResponseEntity.status(500).build();
-        }
+    public ResponseEntity<Void> removeLikes(@Valid @RequestBody IsLikeRequest requestDto){
+        likeService.removeLikes(requestDto);
+        return ResponseEntity.ok().build();
     }
     
     //いいね取得
     @GetMapping("/likes/{userId}")
     public ResponseEntity<List<PostDetailResponse>> getLikedPosts(@PathVariable Integer userId){
         List<PostDetailResponse> likedPosts;
-        try{
-            likedPosts = likeService.getLikedPosts(userId);
-        }catch(Exception e){
-            log.error("いいねした投稿の取得に失敗しました。 userId: {} エラー: {}", userId, e);
-            return ResponseEntity.status(500).build();
-        }
+        
+        likedPosts = likeService.getLikedPosts(userId);
         return ResponseEntity.ok(likedPosts);
     }
 
@@ -208,24 +160,16 @@ public class PostController {
     //検索履歴を削除
     @DeleteMapping("/searchhistory")
     public ResponseEntity<Void> removeSearchHistory(@RequestBody SearchHistoryRemoveRequest requestDto) {
-        try{
-            searchHistoryService.removeSearchHistory(requestDto.getUserId(), requestDto.getKeyword());
-        }catch(Exception e){
-            log.error("検索履歴の削除に失敗しました。", e);
-            return ResponseEntity.status(500).build();
-        }
+        
+        searchHistoryService.removeSearchHistory(requestDto.getUserId(), requestDto.getKeyword());
         return ResponseEntity.ok().build();
     }
     
     //投稿を削除
     @DeleteMapping("/{postId}/user/{userId}")
     public ResponseEntity<Void> deletePost(@PathVariable String postId, @PathVariable Integer userId){
-        try{
-            postService.deletePost(postId, userId);
-        }catch(Exception e){
-            log.error("投稿の削除に失敗しました。", e);
-            return ResponseEntity.status(500).build();
-        }
+        
+        postService.deletePost(postId, userId);
 
         return ResponseEntity.ok().build();
     }
