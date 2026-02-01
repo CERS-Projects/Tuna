9383fd4bdb6c8c0904e1809fd11d8201904bd337
hmuto111 <h.muto111@gmail.com>
2026-02-01 19:05:05 +0900

    feat: ログイン失敗時のエラーアラートの追加 #174

--- frontend/src/features/auth/context/authProvider.tsx
+++ frontend/src/features/auth/context/authProvider.tsx
@@ -5,7 +5,7 @@ import { type LoginInfo, type LoginResponse } from "../types/auth.ts";
 import { useLogin } from "../hooks/useLogin.ts";
 import { useLogout } from "../hooks/useLogout.ts";
 import { useRefreshToken } from "../hooks/useRefreshToken.ts";
-import { useNavigate, useLocation } from "react-router"; // ★ useLocation 追加
+import { useNavigate, useLocation } from "react-router";
 import { paths } from "@/config/paths.ts";
 import { ApiRequestError } from "@/types/apiRequestError.ts";
 import { Spinner } from "@/components/ui/spinner/spinner.tsx";
@@ -48,7 +48,7 @@ export const AuthProvider = ({ children }: Props) => {
         setIsAuthChecking(false);
       },
     });
-  }, [navigate, setIsAuthChecking, refreshMutate, location]);
+  }, [navigate, setIsAuthChecking, refreshMutate]);
 
   const login = (info: LoginInfo) => {
     loginMutate(info, {
@@ -58,8 +58,13 @@ export const AuthProvider = ({ children }: Props) => {
       },
       onError: (error) => {
         if (error instanceof ApiRequestError) {
-          alert(`ログイン失敗: ${error.message}`);
+          if (error.body?.statusCode === 403) {
+            alert(`ログイン失敗: ${error.body.errorMessage}`);
+          } else {
+            alert(`ログイン失敗`);
+          }
         } else {
+          console.error(error);
           alert("予期せぬエラーが発生しました");
         }
       },
