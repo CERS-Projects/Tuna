a17f9b4f5cf89cc14abc318e86a126ae0fe8e3ae
hmuto111 <h.muto111@gmail.com>
2026-02-02 21:39:24 +0900

    feat: imgのalt属性を設定 #96

--- frontend/src/features/post/components/userInfo/userInfo.tsx
+++ frontend/src/features/post/components/userInfo/userInfo.tsx
@@ -9,7 +9,11 @@ type UserInfoProps = {
 export const UserInfo = ({ userIcon, userName, userId }: UserInfoProps) => {
   return (
     <div className={styles.userInfo}>
-      <img src={userIcon} alt="User Icon" className={styles.userIcon} />
+      <img
+        src={userIcon}
+        alt={`${userName}のアイコン`}
+        className={styles.userIcon}
+      />
       <div>
         <div className={styles.userName}>{userName}</div>
         <div className={styles.userId}>@{userId}</div>
