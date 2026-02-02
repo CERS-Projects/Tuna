02569f9830d858b9131dd8be4d41ae44021cf2c8
hmuto111 <h.muto111@gmail.com>
2026-02-02 15:57:33 +0900

    feat: post作成用のAPIを再定義 #96

---
+++ frontend/src/features/post/api/createPost.ts
@@ -0,0 +1,30 @@
+import { api } from "@/lib/api-client";
+import { type CreatePostRequest } from "../types/post";
+
+export const createPost = async (data: CreatePostRequest): Promise<void> => {
+  const formData = new FormData();
+
+  formData.append("sentence", data.sentence);
+
+  data.shareRange.forEach((id) => {
+    formData.append("shareRange", id.toString());
+  });
+
+  if (data.imageFile && data.imageFile.length > 0) {
+    data.imageFile.forEach((file) => {
+      formData.append("imageFile", file, file.name);
+    });
+  }
+
+  if (data.responseTo) {
+    formData.append("responseTo", data.responseTo);
+  }
+
+  await api<void>({
+    url: "/posts",
+    options: {
+      method: "POST",
+      body: formData,
+    },
+  });
+};
