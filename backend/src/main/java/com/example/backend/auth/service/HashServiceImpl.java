b9e38b284e045d626edbceb849248bbffb1b0015
yuki <yuki.kikuchi27@gmail.com>
2026-02-03 14:20:38 +0900

    refactor: @Overrideの追加

--- backend/src/main/java/com/example/backend/auth/service/HashServiceImpl.java
+++ backend/src/main/java/com/example/backend/auth/service/HashServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HashServiceImpl implements HashService {
+    @Override
     public String toHash(String data) {
         String hashData;
         try {
