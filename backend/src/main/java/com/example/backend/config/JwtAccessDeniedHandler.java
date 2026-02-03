f52f409be403c149b3604df913992bd1dc4f19b8
yuki <yuki.kikuchi27@gmail.com>
2026-02-03 12:56:56 +0900

    refactor: フォルダー名変更に伴う変更 #167

--- backend/src/main/java/com/example/backend/config/JwtAccessDeniedHandler.java
+++ backend/src/main/java/com/example/backend/config/JwtAccessDeniedHandler.java
@@ -7,7 +7,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import com.example.backend.exception.Model.ErrorResponseEntity;
+import com.example.backend.exception.model.ErrorResponseEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
