b9c6c72efa50bf72bd4d8dd949d82d431ac14f0e
yuki <yuki.kikuchi27@gmail.com>
2026-02-03 14:19:45 +0900

    feat: ワンタイムパスワードのハッシュ化を変更

--- backend/src/main/java/com/example/backend/auth/service/OtpService.java
+++ backend/src/main/java/com/example/backend/auth/service/OtpService.java
@@ -1,9 +1,10 @@
 package com.example.backend.auth.service;
 
 import com.example.backend.auth.dto.OtpRequest;
+import com.example.backend.auth.dto.OtpResponse;
 
 public interface OtpService {
-    String createOtp(Integer userId);
+    OtpResponse createOtp(Integer userId);
 
     Integer confirmOtp(OtpRequest otpRequest);
 }
--- backend/src/main/java/com/example/backend/auth/service/OtpServiceImpl.java
+++ backend/src/main/java/com/example/backend/auth/service/OtpServiceImpl.java
@@ -1,13 +1,19 @@
 package com.example.backend.auth.service;
 
-import java.util.Random;
+import java.security.SecureRandom;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.backend.auth.dto.OtpRequest;
+import com.example.backend.auth.dto.OtpResponse;
+import com.example.backend.auth.dto.RedisOtp;
 import com.example.backend.exception.AuthException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,45 +22,81 @@ import lombok.RequiredArgsConstructor;
 public class OtpServiceImpl implements OtpService {
 
     private final StringRedisTemplate stringRedisTemplate;
-    private final HashService hashService;
     private final LoginAttemptService loginAttemptService;
+    private final PasswordEncoder passwordEncoder;
+    private final SecureRandom secureRandom;
+    private final ObjectMapper objectMapper;
 
     @Override
-    public String createOtp(Integer userId) {
+    public OtpResponse createOtp(Integer userId) {
+
         String otp;
         String hashOtp;
-        String optKey = userId + "_Otp";
+        String jsonValue;
 
-        Random random = new Random();
+        if (stringRedisTemplate.hasKey(userId + "_OtpToken")) {
+            String oldOtpToken = stringRedisTemplate.opsForValue().get(userId + "_OtpToken");
+            stringRedisTemplate.delete(userId + "_OtpToken");
+            stringRedisTemplate.delete(oldOtpToken);
+        }
+
         int min = 100000;
         int max = 999999;
-        otp = String.valueOf(random.nextInt(max - min + 1) + min);
-        hashOtp = hashService.toHash(otp);
-        stringRedisTemplate.opsForValue().set(optKey, hashOtp, 5, TimeUnit.MINUTES);
-        return otp;
+        otp = String.valueOf(secureRandom.nextInt(max - min + 1) + min);
+        hashOtp = passwordEncoder.encode(otp);
+        RedisOtp redisOtp = new RedisOtp(userId, hashOtp);
+        String otpTokenKey = UUID.randomUUID().toString();
+        if (hashOtp == null) {
+            throw new IllegalStateException("サーバー内部でエラーが発生しました");
+        }
+
+        try {
+            jsonValue = objectMapper.writeValueAsString(redisOtp);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("サーバー内部でエラーが発生しました");
+        }
+        if (otpTokenKey == null || jsonValue == null) {
+            throw new IllegalStateException("サーバー内部でエラーが発生しました");
+        }
+        stringRedisTemplate.opsForValue().set(userId + "_OtpToken", otpTokenKey);
+        stringRedisTemplate.opsForValue().set(otpTokenKey, jsonValue, 5, TimeUnit.MINUTES);
+        OtpResponse otpResponse = new OtpResponse(otpTokenKey, otp);
+        return otpResponse;
     }
 
     @Override
     public Integer confirmOtp(OtpRequest otpRequest) {
-        Integer userId = otpRequest.getUserId();
-        String otpKey = userId + "_Otp";
-        String saveHashOtp = stringRedisTemplate.opsForValue().get(otpKey);
-        String hashOtp = hashService.toHash(otpRequest.getOtp());
+        String requestOtp = otpRequest.getOtp();
+        String otpToken = otpRequest.getOtpToken();
+        if (requestOtp == null || otpToken == null) {
+            throw new IllegalArgumentException("リクエストの値にnullが含まれています");
+        }
+        RedisOtp redisOtp;
+        if (!stringRedisTemplate.hasKey(otpToken)) {
+            throw new AuthException("ログインしなおしてください");
+        }
+        String jsonValue = stringRedisTemplate.opsForValue().get(otpToken);
+        try {
+            redisOtp = objectMapper.readValue(jsonValue, RedisOtp.class);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("サーバー内部でエラーが発生しました");
+        }
 
-        if (saveHashOtp == null) {
+        if (redisOtp == null) {
             throw new AuthException("ログインしなおしてください");
         }
 
-        if (!hashOtp.equals(saveHashOtp)) {
-            loginAttemptService.otpFailed(userId);
+        if (!passwordEncoder.matches(requestOtp, redisOtp.getOtp())) {
+            loginAttemptService.otpFailed(redisOtp.getUserId(), otpToken);
             throw new AuthException("ワンタイムパスワードが異なります");
         }
-        String missOtpKey = userId + "_MissOtp";
-        stringRedisTemplate.delete(otpKey);
+        String missOtpKey = redisOtp.getUserId() + "_MissOtp";
+        stringRedisTemplate.delete(redisOtp.getUserId() + "_OtpToken");
+        stringRedisTemplate.delete(otpToken);
         if (stringRedisTemplate.hasKey(missOtpKey)) {
             stringRedisTemplate.delete(missOtpKey);
         }
-        return userId;
+        return redisOtp.getUserId();
     }
 
 }
