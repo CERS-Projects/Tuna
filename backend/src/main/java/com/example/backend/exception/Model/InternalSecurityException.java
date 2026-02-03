1569e4bebec5df6384174efdae249ed307ee4f11
yuki <yuki.kikuchi27@gmail.com>
2026-02-03 12:53:52 +0900

    refactor: フォルダー名変更に伴う削除 #167

--- backend/src/main/java/com/example/backend/exception/Model/InternalSecurityException.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.exception.Model;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class InternalSecurityException extends AuthenticationException {
-    public InternalSecurityException(String msg) {
-        super(msg);
-    }
-}
