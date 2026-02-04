0ad0698a314f9a285a16267a212645ed129937c8
hmuto111 <h.muto111@gmail.com>
2026-02-04 22:07:50 +0900

    feat: useUserフックを更新し、プロフィール取得時のエラーハンドリングを追加(初回ログインの場合はアカウントを作成する) #195

--- frontend/src/features/auth/components/protectedRoute.tsx
+++ frontend/src/features/auth/components/protectedRoute.tsx
@@ -5,12 +5,13 @@ import { paths } from "@/config/paths";
 
 const ProtectedRoute = () => {
   const { authToken } = useAuth();
-  // const { isError } = useUser(authToken);
+  const { isError } = useUser(authToken);
 
   if (!authToken) return <Navigate to={paths.auth.login.path} replace />;
-  //   if (isError) {
-  //     alert("ユーザ情報の取得に失敗しました。");
-  //   }
+  if (isError) {
+    alert("ユーザ情報の取得に失敗しました。");
+    return <Navigate to={paths.welcome.path} replace />;
+  }
 
   return <Outlet />;
 };
--- frontend/src/features/auth/hooks/useUser.ts
+++ frontend/src/features/auth/hooks/useUser.ts
@@ -4,34 +4,85 @@ import { type ProfileData } from "@/features/profile/types/profileTypes";
 import { type User } from "@/types/user";
 import { decodeUserParams } from "../utils/jwt";
 import { type JWTPayload } from "../types/auth";
+import { ApiRequestError } from "@/types/apiRequestError";
 
 export const useUser = (
   authToken: string,
   options?: Partial<UseQueryOptions<User>>,
 ) => {
-  const user = decodeUserParams(authToken) as JWTPayload;
   const apiWithRefresh = useApiWithRefresh();
+  const jwtPayload = decodeUserParams(authToken) as JWTPayload;
+
   return useQuery({
-    queryKey: ["user", user.sub],
-    queryFn: async () => {
-      const profile = await apiWithRefresh<ProfileData>({
-        url: "/profile",
-        options: {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${authToken}`,
+    queryKey: ["user", jwtPayload?.sub],
+    queryFn: async (): Promise<User> => {
+      try {
+        const profile = await apiWithRefresh<ProfileData>({
+          url: "/profile/me",
+          options: {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${authToken}`,
+            },
           },
-        },
-      });
+        });
 
-      const userInfo = decodeUserParams(authToken);
-      const user: User = { ...profile, role: userInfo?.role ?? "STUDENT" };
+        return {
+          showUserId: profile.showUserId,
+          userName: profile.userName,
+          iconUrl: profile.iconUrl,
+          follow: profile.followCount,
+          follower: profile.followerCount,
+          introduction: profile.introduction,
+          role: jwtPayload?.role ?? "STUDENT",
+        };
+      } catch (error) {
+        if (
+          error instanceof ApiRequestError &&
+          error.statusMessage === "NOT_FOUND"
+        ) {
+          await apiWithRefresh({
+            url: "/profile",
+            options: {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${authToken}`,
+              },
+            },
+          });
 
-      return user;
+          const profile = await apiWithRefresh<ProfileData>({
+            url: "/profile/me",
+            options: {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${authToken}`,
+              },
+            },
+          });
+          alert("プロフィールが作成されていなかったため、作成しました");
+
+          return {
+            showUserId: profile.showUserId,
+            userName: profile.userName,
+            iconUrl: profile.iconUrl,
+            follow: profile.followCount,
+            follower: profile.followerCount,
+            introduction: profile.introduction,
+            role: jwtPayload?.role ?? "STUDENT",
+          };
+        }
+
+        throw error;
+      }
     },
     enabled: !!authToken,
     staleTime: 1000 * 60 * 10,
+    retry: false,
+    throwOnError: false,
     ...options,
   });
 };
