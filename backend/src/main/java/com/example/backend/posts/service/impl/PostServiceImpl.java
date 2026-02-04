7a3322edc4edc667fcf051e9e4c8ce77e782580e
hmuto111 <h.muto111@gmail.com>
2026-02-05 01:47:21 +0900

    fix: shareRangeが全体公開の投稿への返信作成ができるように条件文を追加 #195

--- backend/src/main/java/com/example/backend/posts/service/impl/PostServiceImpl.java
+++ backend/src/main/java/com/example/backend/posts/service/impl/PostServiceImpl.java
@@ -34,7 +34,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class PostServiceImpl implements PostService {
-    
+
     private final PostRepository postRepository;
 
     private final FileControlHelper fileControlHelper;
@@ -48,7 +48,7 @@ public class PostServiceImpl implements PostService {
     private final GroupJoinByUserId groupJoinByUserId;
 
     private final ProfileRepository profileRepository;
-    
+
     // 投稿作成
     @Override
     public void insertPost(PostInsertRequest post, Integer userId) {
@@ -57,47 +57,47 @@ public class PostServiceImpl implements PostService {
         List<MultipartFile> imageFiles = post.getImageFile();
         ObjectId replyPost = null;
 
-
-        //publicの0を除外
-        List<Integer> userGroupIds =
-        post.getShareRange().stream()
+        // publicの0を除外
+        List<Integer> userGroupIds = post.getShareRange().stream()
                 .filter(i -> i != 0)
                 .toList();
-        //除外したリストをもとに権限確認
-        if(!accountConfirm.isExistsAllGroups(userId, userGroupIds.toArray(new Integer[0]))) {
-            throw new IllegalArgumentException("指定されたグループに所属していません。");
+        // 除外したリストをもとに権限確認（userGroupIdsが空でない場合のみ）
+        if (userGroupIds.size() == 0) {
+            if (!accountConfirm.isExistsAllGroups(userId, userGroupIds.toArray(new Integer[0]))) {
+                throw new IllegalArgumentException("指定されたグループに所属していません。");
+            }
         }
 
-        if(imageFiles != null){
+        if (imageFiles != null) {
             fileObjectKeys = fileControlHelper.uploadFile("images", imageFiles.toArray(new MultipartFile[0]));
             log.info("ファイルアップロード成功: {}", String.join(", ", fileObjectKeys));
-            
+
         }
-        //返信投稿設定
+        // 返信投稿設定
         if (post.getResponseTo() != null && !post.getResponseTo().isEmpty()) {
-            replyPost = new ObjectId(post.getResponseTo()); 
+            replyPost = new ObjectId(post.getResponseTo());
             log.info("返信：{}", postRepository.existsById(replyPost));
         }
 
         OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
-        
+
         postEntity.setUserId(userId);
         postEntity.setSentence(post.getSentence());
         postEntity.setPostDate(Date.from(now.toInstant()));
         postEntity.setImageObjectKey(fileObjectKeys);
         postEntity.setShareRange(post.getShareRange());
-        //初期値はtrue
+        // 初期値はtrue
         postEntity.setPostFlag(true);
         postEntity.setLikeCount(0);
         postEntity.setResponseTo(replyPost);
         postEntity.setResponseCount(0);
         try {
-        postRepository.save(postEntity);
-        if (postEntity.getResponseTo() != null) {
+            postRepository.save(postEntity);
+            if (postEntity.getResponseTo() != null) {
                 log.info("返信元投稿のresponse_countをインクリメントします。 投稿ID: {}", replyPost);
                 long result = postRepository.incrementResponseCount(replyPost);
                 if (result == 0) {
-                    log.warn("返信元投稿のresponse_countのインクリメントに失敗しました。該当する投稿が見つかりません。 投稿ID:{} " , replyPost);
+                    log.warn("返信元投稿のresponse_countのインクリメントに失敗しました。該当する投稿が見つかりません。 投稿ID:{} ", replyPost);
                     throw new RuntimeException("返信元投稿のresponse_countのインクリメントに失敗しました。該当する投稿が見つかりません。");
                 } else {
                     log.info("返信元投稿のresponse_countを正常にインクリメントしました。 投稿ID: {}", replyPost);
@@ -106,7 +106,7 @@ public class PostServiceImpl implements PostService {
         } catch (Exception e) {
             // ファイル削除
             log.info("投稿の保存に失敗したため、アップロードしたファイルを削除します。");
-            if(fileObjectKeys != null){
+            if (fileObjectKeys != null) {
                 fileControlHelper.deleteFile(fileObjectKeys.toArray(new String[0]));
             }
             log.error("投稿の保存に失敗しました。", e);
@@ -114,26 +114,24 @@ public class PostServiceImpl implements PostService {
         }
     }
 
-    //タイムライン取得
+    // タイムライン取得
     @Override
     public List<PostDetailResponse> getTimelinePosts(Integer shareRange, Integer currentUserId) {
         List<Integer> shareRangeList = List.of(shareRange);
 
-        if(!(shareRangeList.size() == 1 && shareRangeList.get(0) == 0)){
+        if (!(shareRangeList.size() == 1 && shareRangeList.get(0) == 0)) {
 
-            //publicのみ指定されている場合、全グループ参加確認は不要
+            // publicのみ指定されている場合、全グループ参加確認は不要
             if (!accountConfirm.isExistsAllGroups(currentUserId, shareRangeList.toArray(new Integer[0]))) {
                 throw new IllegalArgumentException("指定されたグループに所属していません。");
             }
         }
 
         try {
-            List<PostDetailResponse> postDetails =
-                postRepository.findPostsWithDetails(
+            List<PostDetailResponse> postDetails = postRepository.findPostsWithDetails(
                     currentUserId,
                     shareRangeList,
-                    getmuteWordList(currentUserId)
-                );
+                    getmuteWordList(currentUserId));
 
             log.info("タイムライン投稿 件数={}", postDetails.size());
             if (!postDetails.isEmpty()) {
@@ -153,7 +151,7 @@ public class PostServiceImpl implements PostService {
         }
     }
 
-    //ユーザー投稿取得
+    // ユーザー投稿取得
     @Override
     public List<PostDetailResponse> getUserPosts(Integer targetUserId, Integer currentUserId) {
         List<PostDetailResponse> postDetails;
@@ -164,36 +162,37 @@ public class PostServiceImpl implements PostService {
         }
         userGroupIds.add(0); // public権限を追加
 
-        try{
-            log.info("取得を開始しました 相手targetUserId: " + targetUserId + " 取得 currentUserId: " + currentUserId );
+        try {
+            log.info("取得を開始しました 相手targetUserId: " + targetUserId + " 取得 currentUserId: " + currentUserId);
             postDetails = postRepository.findUserPostsWithDetails(currentUserId, targetUserId, userGroupIds);
             log.info("取得完了しました。 投稿数: " + postDetails.size());
-        }catch(Exception e){
+        } catch (Exception e) {
 
             log.error("ユーザー投稿の取得に失敗しました。", e.getMessage(), e);
             throw new RuntimeException("ユーザー投稿の取得に失敗しました。", e);
         }
-        for(PostDetailResponse postDetail : postDetails){
+        for (PostDetailResponse postDetail : postDetails) {
             postDetail.setImageUrl(fileControlHelper.getMultiFileUrl(postDetail.getImageUrl()));
             postDetail.setIcon(fileControlHelper.getFileUrl(postDetail.getIcon()));
         }
 
         return postDetails;
     }
-    
-    //返信取得
+
+    // 返信取得
     @Override
-    public List<PostDetailResponse> getReplyPosts(String replyPostId,Integer currentUserId) {
+    public List<PostDetailResponse> getReplyPosts(String replyPostId, Integer currentUserId) {
         List<PostDetailResponse> postDetails;
-        
-        try{
-            log.info("返信取得を開始しました 投稿ID: " + replyPostId + " 取得 currentUserId: " + currentUserId );
-            postDetails = postRepository.findPostsResponseWithDetails(currentUserId, new ObjectId(replyPostId), getmuteWordList(currentUserId));
-        }catch(Exception e){
+
+        try {
+            log.info("返信取得を開始しました 投稿ID: " + replyPostId + " 取得 currentUserId: " + currentUserId);
+            postDetails = postRepository.findPostsResponseWithDetails(currentUserId, new ObjectId(replyPostId),
+                    getmuteWordList(currentUserId));
+        } catch (Exception e) {
             log.error("返信投稿の取得に失敗しました。", e);
             throw new RuntimeException("返信投稿の取得に失敗しました。", e);
         }
-        for(PostDetailResponse postDetail : postDetails){
+        for (PostDetailResponse postDetail : postDetails) {
             postDetail.setImageUrl(fileControlHelper.getMultiFileUrl(postDetail.getImageUrl()));
             postDetail.setIcon(fileControlHelper.getFileUrl(postDetail.getIcon()));
         }
@@ -201,27 +200,28 @@ public class PostServiceImpl implements PostService {
         return postDetails;
     }
 
-    //キーワード検索投稿取得
+    // キーワード検索投稿取得
     @Override
     public List<PostDetailResponse> getPostsByKeyword(String keyword, Integer currentUserId, List<Integer> shareRange) {
         List<PostDetailResponse> postDetails;
 
-        if(!(shareRange.size() == 1 && shareRange.get(0) == 0)){
+        if (!(shareRange.size() == 1 && shareRange.get(0) == 0)) {
             if (!accountConfirm.isExistsAllGroups(currentUserId, shareRange.toArray(new Integer[0]))) {
                 throw new IllegalArgumentException("指定されたグループに所属していません。");
-            }else{
+            } else {
                 log.info("publicのみ指定されているため、グループ所属確認をスキップします。 currentUserId: " + currentUserId);
                 shareRange = List.of(0); // 条件指定がない場合publicの追加
             }
         }
-        try{
+        try {
             log.info("キーワード検索投稿取得を開始しました キーワード: " + keyword + " 取得 currentUserId: " + currentUserId);
-            postDetails = postRepository.findPostsByKeywordWithDetails(currentUserId, keyword, getmuteWordList(currentUserId),shareRange);
-        }catch(Exception e){
+            postDetails = postRepository.findPostsByKeywordWithDetails(currentUserId, keyword,
+                    getmuteWordList(currentUserId), shareRange);
+        } catch (Exception e) {
             log.error("キーワード検索投稿の取得に失敗しました。", e);
             throw new RuntimeException("キーワード検索投稿の取得に失敗しました。", e);
         }
-        for(PostDetailResponse postDetail : postDetails){
+        for (PostDetailResponse postDetail : postDetails) {
             postDetail.setImageUrl(fileControlHelper.getMultiFileUrl(postDetail.getImageUrl()));
             postDetail.setIcon(fileControlHelper.getFileUrl(postDetail.getIcon()));
         }
@@ -235,7 +235,7 @@ public class PostServiceImpl implements PostService {
         // 1) 入力チェック
         if (postId == null || postId.isBlank()) {
             throw new IllegalArgumentException("投稿IDが無効です。");
-        }   
+        }
         if (userId == null) {
             throw new IllegalArgumentException("ユーザーIDが無効です。");
         }
@@ -252,7 +252,7 @@ public class PostServiceImpl implements PostService {
         PostEntity post = postRepository.findById(postObjectId)
                 .orElseThrow(() -> new IllegalArgumentException("指定された投稿が存在しません。"));
 
-        // 4)　ユーザーチェック
+        // 4) ユーザーチェック
         if (!postRepository.existsByIdAndUserId(postObjectId, userId)) {
             throw new IllegalArgumentException("投稿の削除権限がありません。");
         }
@@ -271,18 +271,18 @@ public class PostServiceImpl implements PostService {
             }
 
             // 8) 投稿削除と返信のデクリメント
-            if(  post.getResponseTo() != null){
+            if (post.getResponseTo() != null) {
                 log.info("返信元投稿のresponse_countをデクリメントします。 投稿ID: {}", post.getResponseTo());
                 long result = postRepository.decrementResponseCount(post.getResponseTo());
                 if (result == 0) {
-                    log.warn("返信元投稿のresponse_countのデクリメントに失敗しました。該当する投稿が見つかりません。 投稿ID:{} " , post.getResponseTo());
+                    log.warn("返信元投稿のresponse_countのデクリメントに失敗しました。該当する投稿が見つかりません。 投稿ID:{} ", post.getResponseTo());
                     throw new RuntimeException("返信元投稿のresponse_countのデクリメントに失敗しました。該当する投稿が見つかりません。");
                 } else {
                     log.info("返信元投稿のresponse_countを正常にデクリメントしました。 投稿ID: {}", post.getResponseTo());
                 }
             }
             postRepository.deleteByIdAndUserId(postObjectId, userId);
-            
+
             log.info("投稿の削除に成功しました。 投稿ID: {}", postId);
 
         } catch (Exception e) {
@@ -291,48 +291,45 @@ public class PostServiceImpl implements PostService {
         }
     }
 
-    //投稿の単体取得
+    // 投稿の単体取得
     @Override
     public PostDetailResponse getPostById(String postId, Integer currentUserId) {
         PostDetailResponse postDetail;
-        Set<Integer> postShareRangeList;
 
-        try{
-            log.info("投稿単体取得を開始しました 投稿ID: " + postId + " 取得 currentUserId: " + currentUserId );
-            
+        try {
+            log.info("投稿単体取得を開始しました 投稿ID: " + postId + " 取得 currentUserId: " + currentUserId);
 
             postDetail = postRepository.findPostsWithDetail(
-                new ObjectId(postId),
-                currentUserId
-            );
-            
-            if(postDetail == null){
-                log.error("指定された投稿が存在しません。 投稿ID: " + postId );
+                    new ObjectId(postId),
+                    currentUserId);
+
+            if (postDetail == null) {
+                log.error("指定された投稿が存在しません。 投稿ID: " + postId);
                 throw new RuntimeException("指定された投稿が存在しません。");
             }
-            //投稿閲覧権限確認
-            if(!canViewPost(currentUserId, postDetail.getShareRange())){
-                log.error("投稿の閲覧権限がありません。 投稿ID: " + postId );
+            // 投稿閲覧権限確認
+            if (!canViewPost(currentUserId, postDetail.getShareRange())) {
+                log.error("投稿の閲覧権限がありません。 投稿ID: " + postId);
                 throw new IllegalArgumentException("投稿の閲覧権限がありません。");
             }
-            log.info("取得完了しました。 投稿ID: " + postId );
+            log.info("取得完了しました。 投稿ID: " + postId);
 
             postDetail.setImageUrl(fileControlHelper.getMultiFileUrl(postDetail.getImageUrl()));
             postDetail.setIcon(fileControlHelper.getFileUrl(postDetail.getIcon()));
             return postDetail;
 
-        }catch(Exception e){
+        } catch (Exception e) {
             log.error("投稿の取得に失敗しました。 投稿ID: " + postId, e);
             throw new RuntimeException("投稿の取得に失敗しました。", e);
         }
     }
 
-    //ミュートワードリスト取得
+    // ミュートワードリスト取得
     private List<String> getmuteWordList(Integer userId) {
         List<String> muteWordList;
         UserProfileEntity profile = profileRepository.getFilterWordsByUserId(userId)
-            .orElse(null);
-        
+                .orElse(null);
+
         if (profile == null || profile.getFilterWords() == null) {
             muteWordList = List.of();
         } else {
@@ -341,15 +338,14 @@ public class PostServiceImpl implements PostService {
         log.info("取得したミュートワードリスト: " + muteWordList);
 
         return muteWordList;
-        }
+    }
 
-    //投稿閲覧権限確認
+    // 投稿閲覧権限確認
     private boolean canViewPost(Integer userId, List<Integer> postShareRange) {
-    Set<Integer> userGroups = new HashSet<>(
-        groupJoinByUserId.getJoinedGroupIdsByUserId(userId)
-    );
-    userGroups.add(0);
+        Set<Integer> userGroups = new HashSet<>(
+                groupJoinByUserId.getJoinedGroupIdsByUserId(userId));
+        userGroups.add(0);
 
-    return postShareRange.stream().anyMatch(userGroups::contains);
+        return postShareRange.stream().anyMatch(userGroups::contains);
     }
 }
