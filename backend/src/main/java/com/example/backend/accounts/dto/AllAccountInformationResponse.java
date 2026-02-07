aedfe85bc96ba5c06469e051b9e1bcb9755d02c1
yuki <yuki.kikuchi27@gmail.com>
2026-02-07 18:31:34 +0900

    feat 学校管理者・教師のアカウント検索追加 #196

---
+++ backend/src/main/java/com/example/backend/accounts/dto/AllAccountInformationResponse.java
@@ -0,0 +1,22 @@
+package com.example.backend.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class AllAccountInformationResponse {
+    private Integer userId;
+
+    private String showUserId;
+
+    private String name;
+
+    private Integer grade;
+
+    private Boolean isAdmin;
+
+    private Boolean isAccountStopFlag;
+}
