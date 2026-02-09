c6856990fdc5266dd3e34c2ddddef26c1ae23d54
yuki <yuki.kikuchi27@gmail.com>
2026-02-09 10:16:10 +0900

    feat: 適切なアクセス権限設定 #196

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
                 .logout(logout -> logout.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/refresh", "/otp", "/support/**").permitAll()
-                        .requestMatchers("/school/create/", "/accounts/all").hasAnyRole("TEACHER", "ADMIN_SCHOOL")
+                        .requestMatchers("/school/create", "/accounts/all", "/accounts/teacher/**")
+                        .hasRole("ADMIN_SCHOOL")
                         .requestMatchers("/group/**", "/accounts/student/**", "/notice/create", "/notice/modify",
                                 "/notice/teacher/list", "/notice/delete", "/report/list", "/report/delete")
                         .hasAnyRole("TEACHER", "ADMIN_SCHOOL")
