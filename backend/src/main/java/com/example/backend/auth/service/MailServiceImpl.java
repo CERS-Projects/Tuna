c4d6c3fd5fbcc178ff38ee6b1ab67b5819a88ebb
MattunS0 <mqtsumoto.s0u@gmail.com>
2026-02-10 10:41:54 +0900

    refactor: デバッグ処理の削除 #201

--- backend/src/main/java/com/example/backend/auth/service/MailServiceImpl.java
+++ backend/src/main/java/com/example/backend/auth/service/MailServiceImpl.java
@@ -17,21 +17,15 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public void sendMail(UserEntity userEntity, String otpPassword) {
-        System.out.println("aiueo");
+
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
-            System.out.println("aiueo");
             System.out.println(userEntity.getMailAddress());
             mailMessage.setFrom(userEntity.getMailAddress());
-            System.out.println("aiueo");
             mailMessage.setTo(userEntity.getMailAddress());
-            System.out.println("aiueo");
             mailMessage.setSubject("ワンタイムパスワード発行");
-            System.out.println("aiueo");
             mailMessage.setText("ワンタイムパスワードの期限は5分です\n" + otpPassword);
-            System.out.println("aiueo");
             mailSender.send(mailMessage);
-            System.out.println("aiueo");
         } catch (MailException e) {
             throw new IllegalStateException("メール送信で内部エラーが発生しました、もう一度初めからやり直してください");
         }
