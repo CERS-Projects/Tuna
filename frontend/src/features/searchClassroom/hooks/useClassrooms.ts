4162bd72a0debc7d4cc830f50d59f4a590452177
productD5 <daigo.densiaddress@gmail.com>
2026-02-12 17:12:16 +0900

    feat : バックとの接続用hooksを作成 feature #209

---
+++ frontend/src/features/searchClassroom/hooks/useClassrooms.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { type Classroom } from "../types/SelectClassroom";
+import { useApiWithRefresh } from "@/lib/api-client";
+import { useAuth } from "@/features/auth/hooks/useAuth";
+
+export const useClassrooms = () => {
+	const apiWithRefresh = useApiWithRefresh();
+	const { authToken } = useAuth();
+
+	const { data, isFetching, isError, refetch } = useQuery<Classroom[]>({
+		queryKey: ["classrooms"],
+		enabled: !!authToken,
+		queryFn: async (): Promise<Classroom[]> => {
+			const classrooms = await apiWithRefresh<Classroom[]>({
+				url: `/classroom/list`,
+				options: {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${authToken}`,
+					},
+				},
+			});
+			return classrooms;
+		},
+		refetchOnMount: true,
+	});
+
+	return { data, isFetching, isError, refetch };
+};
