1c5104309462b7bdb6a9266377b2ae12b9f1ece1
Chikaty <jyu254780@gmail.com>
2026-02-13 13:51:49 +0900

    refactor: router.tsxのreportの修正 #163

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -84,255 +84,221 @@ const createAppRouter = (queryClient: QueryClient) => {
           paths.auth.accountLock.path,
           () => import("./routes/auth/accountLock"),
         ),
-        route(
-          paths.app.classroom.path,
-          () => import("./routes/app/searchClassroom"),
-        ),
-        route(
-          paths.app.profile.follow.path,
-          () => import("./routes/app/profile/follow"),
-        ),
-        route(
-          paths.app.profile.follower.path,
-          () => import("./routes/app/profile/follower"),
-        ),
-        route(
-          paths.app.classroom.contents.path,
-          () => import("./routes/app/classroom/material"),
-        ),
+
+        // アプリ内ルート (ログイン必要)
         {
-          path: paths.app.profile.root.path,
-          lazy: () =>
-            import("../features/profile/layout/profileLayout").then(
-              convert(queryClient),
-            ),
+          path: paths.app.root.path,
+          element: <ProtectedRoute />,
           children: [
-            route(
-              paths.app.profile.posts.path,
-              () => import("./routes/app/profile/posts"),
-            ),
-            route(
-              paths.app.profile.responses.path,
-              () => import("./routes/app/profile/responses"),
-            ),
-            route(
-              paths.app.profile.goods.path,
-              () => import("./routes/app/profile/goods"),
-            ),
-            route(
-              paths.app.profile.bookmarks.path,
-              () => import("./routes/app/profile/bookmarks"),
-            ),
+            {
+              element: <AppRoot />,
+              ErrorBoundary: AppRootErrorBoundary,
+              children: [
+                {
+                  index: true,
+                  element: <Navigate to={paths.app.timeline.path} replace />,
+                },
+                route(
+                  paths.app.report.root.path,
+                  () => import("./routes/app/report"),
+                ),
+                route(
+                  paths.app.report.confirm.path,
+                  () => import("./routes/app/reportConfirm"),
+                ),
+                route(paths.app.test.path, () => import("./routes/app/test")),
+                {
+                  path: paths.app.timeline.path,
+                  lazy: () =>
+                    import("./routes/app/timeline").then(convert(queryClient)),
+                  children: [
+                    route(
+                      paths.app.timeline.post.path,
+                      () => import("./routes/app/post"),
+                    ),
+                  ],
+                },
+                route(
+                  paths.app.timeline.detail.path,
+                  () => import("./routes/app/detailPost"),
+                ),
+                route(
+                  paths.app.searchPost.path,
+                  () => import("./routes/app/searchPost"),
+                ),
+                route(
+                  paths.app.classroom.path,
+                  () => import("./routes/app/searchClassroom"),
+                ),
+                route(
+                  paths.app.profile.follow.path,
+                  () => import("./routes/app/profile/follow"),
+                ),
+                route(
+                  paths.app.profile.follower.path,
+                  () => import("./routes/app/profile/follower"),
+                ),
+                route(
+                  paths.app.classroom.contents.path,
+                  () => import("./routes/app/classroom/material"),
+                ),
+                {
+                  path: paths.app.profile.root.path,
+                  lazy: () =>
+                    import("../features/profile/layout/profileLayout").then(
+                      convert(queryClient),
+                    ),
+                  children: [
+                    route(
+                      paths.app.profile.posts.path,
+                      () => import("./routes/app/profile/posts"),
+                    ),
+                    route(
+                      paths.app.profile.responses.path,
+                      () => import("./routes/app/profile/responses"),
+                    ),
+                    route(
+                      paths.app.profile.goods.path,
+                      () => import("./routes/app/profile/goods"),
+                    ),
+                    route(
+                      paths.app.profile.bookmarks.path,
+                      () => import("./routes/app/profile/bookmarks"),
+                    ),
+                  ],
+                },
+                route(
+                  paths.app.profile.edit.path,
+                  () => import("./routes/app/profile/edit/editProfile"),
+                ),
+                route(
+                  paths.app.profile.setting.path,
+                  () => import("./routes/app/profile/setting/settingMenu"),
+                ),
+                route(
+                  paths.app.profile.setting.editPassword.path,
+                  () => import("./routes/app/profile/edit/editPassword"),
+                ),
+                route(
+                  paths.app.profile.setting.editFilter.path,
+                  () => import("./routes/app/profile/edit/editFilter"),
+                ),
+              ],
+            },
           ],
         },
-      ],
-    },
 
-    // アプリ内ルート (ログイン必要)
-    {
-      path: paths.app.root.path,
-      element: <ProtectedRoute />,
-      children: [
+        // アプリ内教師ルート (ログイン必要)
         {
-          element: <AppRoot />,
-          ErrorBoundary: AppRootErrorBoundary,
+          path: paths.app.management.root.path,
+          element: <TeacherProtectedRoute />,
           children: [
             {
-              index: true,
-              element: <Navigate to={paths.app.timeline.path} replace />,
-            },
-            route(paths.app.test.path, () => import("./routes/app/test")),
-            {
-              path: paths.app.timeline.path,
-              lazy: () =>
-                import("./routes/app/timeline").then(convert(queryClient)),
-              children: [
-                route(
-                  paths.app.timeline.post.path,
-                  () => import("./routes/app/post"),
-                ),
-              ],
-            },
-            route(
-              paths.app.timeline.detail.path,
-              () => import("./routes/app/detailPost"),
-            ),
-            route(
-              paths.app.searchPost.path,
-              () => import("./routes/app/searchPost"),
-            ),
-            route(
-              paths.app.classroom.path,
-              () => import("./routes/app/searchClassroom"),
-            ),
-            route(
-              paths.app.profile.follow.path,
-              () => import("./routes/app/profile/follow"),
-            ),
-            route(
-              paths.app.profile.follower.path,
-              () => import("./routes/app/profile/follower"),
-            ),
-            route(
-              paths.app.classroom.contents.path,
-              () => import("./routes/app/classroom/material"),
-            ),
-            {
-              path: paths.app.profile.root.path,
-              lazy: () =>
-                import("../features/profile/layout/profileLayout").then(
-                  convert(queryClient),
-                ),
-              children: [
-                route(
-                  paths.app.profile.posts.path,
-                  () => import("./routes/app/profile/posts"),
-                ),
-                route(
-                  paths.app.profile.responses.path,
-                  () => import("./routes/app/profile/responses"),
-                ),
-                route(
-                  paths.app.profile.goods.path,
-                  () => import("./routes/app/profile/goods"),
-                ),
-                route(
-                  paths.app.profile.bookmarks.path,
-                  () => import("./routes/app/profile/bookmarks"),
-                ),
-              ],
-            },
-            route(
-              paths.app.profile.edit.path,
-              () => import("./routes/app/profile/edit/editProfile"),
-            ),
-            route(
-              paths.app.profile.setting.path,
-              () => import("./routes/app/profile/setting/settingMenu"),
-            ),
-            route(
-              paths.app.profile.setting.editPassword.path,
-              () => import("./routes/app/profile/edit/editPassword"),
-            ),
-            route(
-              paths.app.profile.setting.editFilter.path,
-              () => import("./routes/app/profile/edit/editFilter"),
-            ),
-            route(
-              paths.app.report.root.path,
-              () => import("./routes/app/report"),
-            ),
-            route(
-              paths.app.report.confirm.path,
-              () => import("./routes/app/reportConfirm"),
-            ),
-          ],
-        },
-      ],
-    },
-
-    // アプリ内教師ルート (ログイン必要)
-    {
-      path: paths.app.management.root.path,
-      element: <TeacherProtectedRoute />,
-      children: [
-        {
-          element: <ManagementRoot />,
-          ErrorBoundary: ManagementErrorBoundary,
-          children: [
-            {
-              index: true,
-              element: (
-                <Navigate to={paths.app.management.group.root.path} replace />
-              ),
-            },
-            {
-              path: paths.app.management.group.root.path,
-              lazy: () =>
-                import("../features/management/layouts/groupShell/groupShell").then(
-                  convert(queryClient),
-                ),
-              children: [
-                route(
-                  paths.app.management.group.root.path,
-                  () => import("./routes/app/management/group/groupList"),
-                ),
-                route(
-                  paths.app.management.group.new.path,
-                  () => import("./routes/app/management/group/groupNew"),
-                ),
-                route(
-                  paths.app.management.group.edit.path,
-                  () => import("./routes/app/management/group/groupEdit"),
-                ),
-              ],
-            },
-            {
-              path: paths.app.management.account.root.path,
-              lazy: () =>
-                import("../features/management/layouts/accountShell/accountShell").then(
-                  convert(queryClient),
-                ),
+              element: <ManagementRoot />,
+              ErrorBoundary: ManagementErrorBoundary,
               children: [
                 {
                   index: true,
                   element: (
                     <Navigate
-                      to={paths.app.management.account.list.path}
+                      to={paths.app.management.group.root.path}
                       replace
                     />
                   ),
                 },
-                route(
-                  paths.app.management.account.list.path,
-                  () => import("./routes/app/management/account/accountList"),
-                ),
-                route(
-                  paths.app.management.account.edit.path,
-                  () => import("./routes/app/management/account/accountEdit"),
-                ),
-                route(
-                  paths.app.management.account.new.path,
-                  () => import("./routes/app/management/account/accountNew"),
-                ),
-                route(
-                  paths.app.management.account.register.path,
-                  () =>
-                    import("./routes/app/management/account/accountRegister"),
-                ),
-                route(
-                  paths.app.management.account.import.path,
-                  () => import("./routes/app/management/account/accountImport"),
-                ),
-              ],
-            },
-            {
-              path: paths.app.management.classroom.root.path,
-              children: [
                 {
-                  index: true,
-                  element: (
-                    <Navigate
-                      to={paths.app.management.classroom.list.path}
-                      replace
-                    />
-                  ),
+                  path: paths.app.management.group.root.path,
+                  lazy: () =>
+                    import("../features/management/layouts/groupShell/groupShell").then(
+                      convert(queryClient),
+                    ),
+                  children: [
+                    route(
+                      paths.app.management.group.root.path,
+                      () => import("./routes/app/management/group/groupList"),
+                    ),
+                    route(
+                      paths.app.management.group.new.path,
+                      () => import("./routes/app/management/group/groupNew"),
+                    ),
+                    route(
+                      paths.app.management.group.edit.path,
+                      () => import("./routes/app/management/group/groupEdit"),
+                    ),
+                  ],
                 },
-                route(
-                  paths.app.management.classroom.list.path,
-                  () =>
-                    import("./routes/app/management/classroom/classroomList"),
-                ),
-                route(
-                  paths.app.management.classroom.edit.path,
-                  () =>
-                    import("./routes/app/management/classroom/classroomEdit"),
-                ),
-                route(
-                  paths.app.management.classroom.new.path,
-                  () =>
-                    import("./routes/app/management/classroom/classroomNew"),
-                ),
+                {
+                  path: paths.app.management.account.root.path,
+                  lazy: () =>
+                    import("../features/management/layouts/accountShell/accountShell").then(
+                      convert(queryClient),
+                    ),
+                  children: [
+                    {
+                      index: true,
+                      element: (
+                        <Navigate
+                          to={paths.app.management.account.list.path}
+                          replace
+                        />
+                      ),
+                    },
+                    route(
+                      paths.app.management.account.list.path,
+                      () =>
+                        import("./routes/app/management/account/accountList"),
+                    ),
+                    route(
+                      paths.app.management.account.edit.path,
+                      () =>
+                        import("./routes/app/management/account/accountEdit"),
+                    ),
+                    route(
+                      paths.app.management.account.new.path,
+                      () =>
+                        import("./routes/app/management/account/accountNew"),
+                    ),
+                    route(
+                      paths.app.management.account.register.path,
+                      () =>
+                        import("./routes/app/management/account/accountRegister"),
+                    ),
+                    route(
+                      paths.app.management.account.import.path,
+                      () =>
+                        import("./routes/app/management/account/accountImport"),
+                    ),
+                  ],
+                },
+                {
+                  path: paths.app.management.classroom.root.path,
+                  children: [
+                    {
+                      index: true,
+                      element: (
+                        <Navigate
+                          to={paths.app.management.classroom.list.path}
+                          replace
+                        />
+                      ),
+                    },
+                    route(
+                      paths.app.management.classroom.list.path,
+                      () =>
+                        import("./routes/app/management/classroom/classroomList"),
+                    ),
+                    route(
+                      paths.app.management.classroom.edit.path,
+                      () =>
+                        import("./routes/app/management/classroom/classroomEdit"),
+                    ),
+                    route(
+                      paths.app.management.classroom.new.path,
+                      () =>
+                        import("./routes/app/management/classroom/classroomNew"),
+                    ),
+                  ],
+                },
               ],
             },
           ],
