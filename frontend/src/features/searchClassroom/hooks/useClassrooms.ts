7e97d5f27337895f5e579303369fe8246995939b
productD5 <daigo.densiaddress@gmail.com>
2026-02-15 00:00:01 +0900

    feat : ルームの取得、作成、更新、削除のfetch関数を作成 feature #209

--- frontend/src/features/searchClassroom/hooks/useClassrooms.ts
+++ frontend/src/features/searchClassroom/hooks/useClassrooms.ts
@@ -4,27 +4,51 @@ import { useApiWithRefresh } from "@/lib/api-client";
 import { useAuth } from "@/features/auth/hooks/useAuth";
 
 export const useClassrooms = () => {
-	const apiWithRefresh = useApiWithRefresh();
+	const apiWithRefresh =
+		useApiWithRefresh();
 	const { authToken } = useAuth();
 
-	const { data, isFetching, isError, refetch } = useQuery<Classroom[]>({
+	const {
+		data,
+		isFetching,
+		isError,
+		refetch,
+	} = useQuery<Classroom[]>({
 		queryKey: ["classrooms"],
 		enabled: !!authToken,
-		queryFn: async (): Promise<Classroom[]> => {
-			const classrooms = await apiWithRefresh<Classroom[]>({
-				url: `/classroom/list`,
-				options: {
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${authToken}`,
+		queryFn: async (): Promise<
+			Classroom[]
+		> => {
+			const classrooms =
+				await apiWithRefresh<Classroom[]>({
+					url: `/classroom/list`,
+					options: {
+						method: "GET",
+						headers: {
+							"Content-Type":
+								"application/json",
+							Authorization: `Bearer ${authToken}`,
+						},
 					},
-				},
+				});
+			classrooms.map((classroom) => {
+				classroom.latestUpdate = new Date(
+					classroom.latestUpdate,
+				).toLocaleDateString("ja-JP", {
+					year: "numeric",
+					month: "2-digit",
+					day: "2-digit",
+				});
 			});
 			return classrooms;
 		},
 		refetchOnMount: true,
 	});
 
-	return { data, isFetching, isError, refetch };
+	return {
+		data,
+		isFetching,
+		isError,
+		refetch,
+	};
 };
