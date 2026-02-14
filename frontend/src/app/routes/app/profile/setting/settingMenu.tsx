de57a8165c6f08478ab61e1586026208e0ffe504
hmuto111 <h.muto111@gmail.com>
2026-02-15 00:32:27 +0900

    feat: menuバーにユーザ設定への遷移を追加と設定画面でのパス指定を調整 #208

--- frontend/src/app/routes/app/profile/setting/settingMenu.tsx
+++ frontend/src/app/routes/app/profile/setting/settingMenu.tsx
@@ -9,13 +9,13 @@ import { paths } from "@/config/paths";
 const MenuItems: MenuItemType[] = [
   {
     menuName: "パスワード更新",
-    menuPath: paths.app.profile.settingMenu.editPassword.getHref(),
+    menuPath: paths.app.profile.setting.editPassword.getHref(),
     menuIcon: <RiLockPasswordFill />,
     menuLabel: "パスワードの変更を行います",
   },
   {
     menuName: "フィルタリング設定",
-    menuPath: paths.app.profile.settingMenu.editFilter.getHref(),
+    menuPath: paths.app.profile.setting.editFilter.getHref(),
     menuIcon: <IoFilterOutline />,
     menuLabel: "フィルタリングの設定を行います",
   },
--- frontend/src/components/ui/menu/menu.tsx
+++ frontend/src/components/ui/menu/menu.tsx
@@ -1,10 +1,13 @@
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
-import { IoSchoolOutline } from "react-icons/io5";
 import { LuCirclePlus } from "react-icons/lu";
-import { AiOutlineSolution } from "react-icons/ai";
-import { BsBookmark } from "react-icons/bs";
+import {
+  FaHome,
+  FaSearch,
+  FaChalkboardTeacher,
+  FaIdCard,
+  FaBookmark,
+  FaCog,
+} from "react-icons/fa";
 import { useNavigate } from "react-router";
-import { PiHouseLight } from "react-icons/pi";
 import styles from "./menu.module.css";
 import { paths } from "@/config/paths";
 import { useUser } from "@/features/auth/hooks/useUser";
@@ -21,21 +24,21 @@ export const Menu = () => {
         onClick={() => navigate(paths.app.timeline.path)}
         title="タイムライン"
       >
-        <PiHouseLight />
+        <FaHome />
       </button>
       <button
         onClick={() => navigate(paths.app.searchPost.path)}
         className={styles.menuButton}
         title="検索"
       >
-        <HiOutlineMagnifyingGlass />
+        <FaSearch />
       </button>
       <button
         onClick={() => navigate(paths.app.classroom.path)}
         className={styles.menuButton}
         title="授業ルーム"
       >
-        <IoSchoolOutline />
+        <FaChalkboardTeacher />
       </button>
       <button
         onClick={() => navigate(paths.app.timeline.post.path)}
@@ -51,7 +54,7 @@ export const Menu = () => {
         className={styles.menuButton}
         title="プロフィール"
       >
-        <AiOutlineSolution />
+        <FaIdCard />
       </button>
       <button
         onClick={() =>
@@ -62,7 +65,14 @@ export const Menu = () => {
         className={styles.menuButton}
         title="ブックマーク"
       >
-        <BsBookmark />
+        <FaBookmark />
+      </button>
+      <button
+        onClick={() => navigate(paths.app.profile.setting.path)}
+        className={styles.menuButton}
+        title="設定"
+      >
+        <FaCog />
       </button>
     </div>
   );
