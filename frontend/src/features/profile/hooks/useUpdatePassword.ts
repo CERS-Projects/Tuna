cc4d6822a15790e2a72f370584711d277b42b7aa
hmuto111 <h.muto111@gmail.com>
2026-02-15 12:29:54 +0900

    feat: ログイン中のパスワード更新機能の実装,パスワード更新UIの調整 #208

---
+++ frontend/src/features/profile/hooks/useUpdatePassword.ts
@@ -0,0 +1,33 @@
+import { useAuth } from "@/features/auth/hooks/useAuth";
+import { useMutation } from "@tanstack/react-query";
+import { useApiWithRefresh } from "@/lib/api-client";
+import { type EditPasswordForm } from "../types/setting";
+
+export const useUpdatePassword = () => {
+  const { authToken, logout } = useAuth();
+
+  const apiWithRefresh = useApiWithRefresh();
+
+  return useMutation({
+    mutationFn: async (data: EditPasswordForm) => {
+      return await apiWithRefresh<void>({
+        url: `/change/password`,
+        options: {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authToken}`,
+          },
+          body: JSON.stringify(data),
+        },
+      });
+    },
+    onSuccess: () => {
+      alert("パスワードが更新されました。\n再度ログインをお願いします。");
+      logout();
+    },
+    onError: () => {
+      alert("パスワード更新に失敗しました");
+    },
+  });
+};
---
+++ frontend/src/features/profile/types/setting.ts
@@ -0,0 +1,5 @@
+export type EditPasswordForm = {
+  password: string;
+  newPassword: string;
+  confirmPassword: string;
+};
