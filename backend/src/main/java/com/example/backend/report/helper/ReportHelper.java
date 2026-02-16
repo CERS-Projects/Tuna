20f4ebb25aebc62e78825aab9df444b5ebe60a42
productD5 <daigo.densiaddress@gmail.com>
2026-02-16 13:27:49 +0900

    feat : 通報取得のresponseに通報者の追加 feature #226

--- backend/src/main/java/com/example/backend/report/helper/ReportHelper.java
+++ backend/src/main/java/com/example/backend/report/helper/ReportHelper.java
@@ -63,6 +63,12 @@ public class ReportHelper {
         return reportEntities.stream().map(entity -> {
             ReportListResponse response = new ReportListResponse();
             GetUserName object = userRepository.findUserName(entity.getReportedUser());
+
+            // 通報したユーザーの情報を取得
+            GetUserName reportByUser = userRepository.findUserName(entity.getReportBy());
+            if (reportByUser == null) {
+                throw new IllegalArgumentException("通報者が存在しません。");
+            }
             if (object == null) {
                 throw new IllegalArgumentException("そのユーザは存在しないか、報告が存在しません。");
             }
@@ -75,6 +81,8 @@ public class ReportHelper {
             response.setReportDate(entity.getReportDate());
             response.setReportedPost(post.getSentence());
             response.setReportedPostDate(post.getPostDate());
+            response.setReportByName(reportByUser.name());
+            response.setReportByShowUserId(reportByUser.showUserId());
             response.setReportDetail(entity.getDetail());
             return response;
         }).toList();
